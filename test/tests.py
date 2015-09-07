cd313ce20d937bf2b069bc6c940f93c4f8b39ef8
Andrew Stocker <amstocker@dons.usfca.edu>
2015-09-06 17:47:25 -0700

    unit testing

--- test/tests.py
+++ test/tests.py
@@ -1,9 +1,16 @@
+import unittest
+
 import sys
 sys.path.append('..')
 
 import ipfsApi
 
-fake_dir = [{'Hash': u'QmQcCtMgLVwvMQGu6mvsRYLjwqrZJcYtH4mboM9urWW9vX',
+
+class IpfsApiTest(unittest.TestCase):
+
+    api = ipfsApi.Client()
+    
+    fake = [{'Hash': u'QmQcCtMgLVwvMQGu6mvsRYLjwqrZJcYtH4mboM9urWW9vX',
              'Name': 'fake_dir/fsdfgh'},
             {'Hash': u'QmYAhvKYu46rh5NcHzeu6Bhc7NG9SqkF9wySj2jvB74Rkv',
              'Name': 'fake_dir/popoiopiu'},
@@ -20,32 +27,54 @@ fake_dir = [{'Hash': u'QmQcCtMgLVwvMQGu6mvsRYLjwqrZJcYtH4mboM9urWW9vX',
             {'Hash': u'QmbZuss6aAizLEAt2Jt2BD29oq4XfMieGezi6mN4vz9g9A',
              'Name': 'fake_dir'}]
 
-fake_dir_lookup = {i['Name']: i['Hash'] for i in fake_dir}
+    fake_lookup = {i['Name']: i['Hash'] for i in fake}
+  
+
+    ## test_add_multiple_from_list
+    fake_file  = 'fake_dir/fsdfgh'
+    fake_file2 = 'fake_dir/popoiopiu'
+    fake_files_res = [{u'Hash': u'QmQcCtMgLVwvMQGu6mvsRYLjwqrZJcYtH4mboM9urWW9vX',
+                       u'Name': u'fsdfgh'},
+                      {u'Hash': u'QmYAhvKYu46rh5NcHzeu6Bhc7NG9SqkF9wySj2jvB74Rkv',
+                       u'Name': u'popoiopiu'},
+                      {u'Hash': u'QmVkNdzCBukBRdpyFiKPyL2R15qPExMr9rV9RFV2kf9eeV',
+                       u'Name': u''}]
+    
+    ## test_add_multiple_from_dirname
+    fake_dir = 'fake_dir/test2'
+    fake_dir_res = [{u'Hash': u'QmNuvmuFeeWWpxjCQwLkHshr8iqhGLWXFzSGzafBeawTTZ',
+                     u'Name': u'llllg'},
+                    {u'Hash': u'Qmb1NPqPzdHCMvHRfCkk6TWLcnpGJ71KnafacCMm6TKLcD',
+                     u'Name': u'fssdf'},
+                    {u'Hash': u'QmX1dd5DtkgoiYRKaPQPTCtXArUu4jEZ62rJBUcd5WhxAZ',
+                     u'Name': u''}]
+
+
+    #########
+    # TESTS #
+    #########
+
+    def test_add_single_from_str(self):
+        res = self.api.add(self.fake_file)
+        self.assertEqual(res[u"Hash"], self.fake_lookup[self.fake_file])
+    
+    def test_add_single_from_fp(self):
+        with open(self.fake_file, 'rb') as fp:
+            res = self.api.add(fp)
+            self.assertEqual(res[u"Hash"], self.fake_lookup[self.fake_file])
+    
+    def test_add_multiple_from_list(self):
+        res = self.api.add([self.fake_file, self.fake_file2])
+        self.assertEqual(res, self.fake_files_res)
+
+    def test_add_multiple_from_dirname(self):
+        res = self.api.add(self.fake_dir)
+        self.assertEqual(res, self.fake_dir_res)
+
+    def test_add_recursive(self):
+        pass
+
 
 
 if __name__ == "__main__":
-    api = ipfsApi.Client()
-    
-    test_file = 'fake_dir/fsdfgh'
-    test_file2 = 'fake_dir/popoiopiu'
-
-    ## api.add ##
-    res = api.add(test_file)
-    assert res[u"Hash"] == fake_dir_lookup[test_file]
-    
-    res = api.add(['fake_dir/fsdfgh', 'fake_dir/popoiopiu'])
-    assert res == [{u'Hash': u'QmQcCtMgLVwvMQGu6mvsRYLjwqrZJcYtH4mboM9urWW9vX',
-                    u'Name': u'fsdfgh'},
-                   {u'Hash': u'QmYAhvKYu46rh5NcHzeu6Bhc7NG9SqkF9wySj2jvB74Rkv',
-                    u'Name': u'popoiopiu'},
-                   {u'Hash': u'QmVkNdzCBukBRdpyFiKPyL2R15qPExMr9rV9RFV2kf9eeV', u'Name': u''}]
-    
-    res = api.add('fake_dir/test2')
-    assert res == [{u'Hash': u'QmNuvmuFeeWWpxjCQwLkHshr8iqhGLWXFzSGzafBeawTTZ',
-                    u'Name': u'llllg'},
-                   {u'Hash': u'Qmb1NPqPzdHCMvHRfCkk6TWLcnpGJ71KnafacCMm6TKLcD',
-                    u'Name': u'fssdf'},
-                   {u'Hash': u'QmX1dd5DtkgoiYRKaPQPTCtXArUu4jEZ62rJBUcd5WhxAZ', u'Name': u''}]
-
-    print api.add('fake_dir')
-    print api.add('fake_dir', recursive=True)
+    unittest.main()
