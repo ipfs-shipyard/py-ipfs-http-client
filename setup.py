1b5199635e023d65a89c49cfacf9f36145751122
Alex Willmer <alex@moreati.org.uk>
2015-09-12 16:26:23 +0100

    Python 3.x: Declare the packge supports Python 3.3+
    Support 3.2 would require more invasive changes
    e.g. to eliminate all uses of u'' string literals

--- setup.py
+++ setup.py
@@ -41,10 +41,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        #'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3',
         #'Programming Language :: Python :: 3.2',
-        #'Programming Language :: Python :: 3.3',
-        #'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 
     # What does your project relate to?
