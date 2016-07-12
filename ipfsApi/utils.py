9c63984099d0960db7efe9bf29c36961fe809741
Luke Stack <stackjl@appstate.edu>
2016-07-12 10:33:39 -0400

    Added documentation and unit tests for utils.py
    License: MIT
    Signed-off-by: Luke Stack <stackjl@appstate.edu>

--- ipfsApi/utils.py
+++ ipfsApi/utils.py
@@ -1,3 +1,28 @@
+"""A module to handle generic operations.
+
+Classes:
+return_field -- a decorator that returns the given field of a json response
+
+Functions:
+make_string_buffer -- returns a readable/writeable file-like object,
+                      containing a given string value
+encode_json -- returns a serialized Python object as a JSON encoded string
+parse_json -- returns a Python object unserialized from JSON in json_str
+make_json_buffer -- returns a file-like object containing a Python object
+                    serialized to a JSON formatted string
+encode_pyobj -- returns a serialized Python object as a Pickle encoded string
+parse_pyobj -- returns a Python object unserialized from a
+               Pickle encoded string
+make_pyobj_buffer -- returns a file-like object containing a Python object
+                     serialized to a Pickle formatted string
+guess_mimetype -- guesses the mimetype of a file based on the filename
+ls_dir -- returns files and subdirectories within a given directory
+clean_file -- returns a tuple containing a file-like object and a boolean value
+              indicating whether the file needs to be closed after use
+clean_files -- generates tuples with a file-like object and a close indicator
+file_size -- returns the size of a file in bytes
+"""
+
 from __future__ import absolute_import
 
 import io
@@ -25,6 +50,9 @@ def make_string_buffer(string):
     >>> f = make_string_buffer(b'bytes')
     >>> f.read() == b'bytes'
     True
+
+    Keyword arguments:
+    string -- desired content for the returned file-like object
     """
     if isinstance(string, six.text_type):
         buf = StringIO()
@@ -35,41 +63,60 @@ def make_string_buffer(string):
     return buf
 
 
-def encode_json(obj):
-    return json.dumps(obj)
+def encode_json(py_obj):
+    """Returns a serialized Python object as a JSON encoded string.
+
+    Keyword arguments:
+    py_obj -- any given Python object
+    """
+    return json.dumps(py_obj)
 
 
 def parse_json(json_str):
-    """Returns a Python object unserialized from JSON in json_str
+    """Returns a Python object unserialized from JSON in json_str.
 
     >>> parse_json('[1, 2, 3, true, 4.5, null, 6e3]')
     [1, 2, 3, True, 4.5, None, 6000.0]
+
+    Keyword arguments:
+    json_str -- a JSON encoded string from which to reconstruct a Python object
     """
     return json.loads(json_str)
 
 
-def make_json_buffer(json_obj):
-    """Returns a file-like object containing json_obj serialized to JSON
+def make_json_buffer(py_obj):
+    """Returns a file-like object containing py_obj as a JSON encoded string.
 
     >>> f = make_json_buffer([1, 2, 3, True, {u'distance': 4.5}])
     >>> f.read()
     '[1, 2, 3, true, {"distance": 4.5}]'
+
+    Keyword arguments:
+    py_obj -- any given Python object
     """
-    return make_string_buffer(json.dumps(json_obj))
+    return make_string_buffer(encode_json(py_obj))
 
 
 def encode_pyobj(py_obj):
+    """Returns a serialized Python object as a Pickle encoded string.
+
+    Keyword arguments:
+    py_obj -- any given Python object
+    """
     return pickle.dumps(py_obj)
 
 
 def parse_pyobj(pickled):
-    r"""Returns a Python object unpickled from the provided string
+    r"""Returns a Python object unserialized from a Pickle encoded string.
 
     >>> parse_pyobj(b'(lp0\nI1\naI2\naI3\naI01\naF4.5\naNaF6000.0\na.')
     [1, 2, 3, True, 4.5, None, 6000.0]
 
     >>> parse_pyobj(u'(lp0\nI1\naI2\naI3\naI01\naF4.5\naNaF6000.0\na.')
     [1, 2, 3, True, 4.5, None, 6000.0]
+
+    Keyword arguments:
+    pickled -- the string of a pickled Python object to be reconstructed
     """
     if isinstance(pickled, six.text_type):
         pickled = pickled.encode('latin_1')
@@ -77,32 +124,41 @@ def parse_pyobj(pickled):
 
 
 def make_pyobj_buffer(py_obj):
-    """Returns a file-like object containing py_obj serialized to a pickle
+    """Returns a file-like object containing py_obj as a Pickle encoded string.
 
     >>> f = make_pyobj_buffer([1, 2, 3, True, 4.5, None, 6e3])
     >>> isinstance(f.read(), bytes)
     True
+
+    Keyword arguments:
+    py_obj -- any given Python object
     """
-    return make_string_buffer(pickle.dumps(py_obj))
+    return make_string_buffer(encode_pyobj(py_obj))
 
 
 def guess_mimetype(filename):
-    """
-    Guesses the mimetype of a file, based on the filename
+    """Guesses the mimetype of a file based on the filename.
 
     >>> guess_mimetype('example.txt')
     'text/plain'
     >>> guess_mimetype('/foo/bar/example')
     'application/octet-stream'
+
+    Keyword arguments:
+    filename -- the file for which the mimetype is to be determined
     """
     fn = os.path.basename(filename)
     return mimetypes.guess_type(fn)[0] or 'application/octet-stream'
 
 
 def ls_dir(dirname):
-    """
+    """Returns files and subdirectories within a given directory.
+
     Returns a pair of lists, containing the names of directories and files
     in dirname
+
+    Keyword arguments:
+    dirname -- directory to be listed
     """
     ls = os.listdir(dirname)
     files = [p for p in ls if os.path.isfile(os.path.join(dirname, p))]
@@ -111,8 +167,13 @@ def ls_dir(dirname):
 
 
 def clean_file(f):
-    """
-    Returns a file object.
+    """Returns a tuple containing a file-like object and a close indicator.
+
+    This ensures the given file is opened and keeps track of files that should
+    be closed after use (files that were not open prior to this function call).
+
+    Keyword arguments:
+    f -- a filepath or file-like object that may or may not need to be opened
     """
     if not hasattr(f, 'read'):
         return open(f, 'rb'), True
@@ -121,10 +182,15 @@ def clean_file(f):
 
 
 def clean_files(files):
-    """
+    """Generates tuples with a file-like object and a close indicator.
+
     This is a generator of tuples, where the first element is the file object
-    and the second element is a boolean which is True is this module opened the
+    and the second element is a boolean which is True if this module opened the
     file (and thus should close it).
+
+    Keyword arguments:
+    files -- a list or tuple of filepaths and file-like objects
+             or a singular instance of either a filepath or a file-like object
     """
     if isinstance(files, (list, tuple)):
         for f in files:
@@ -134,8 +200,10 @@ def clean_files(files):
 
 
 def file_size(f):
-    """
-    Returns size of file in bytes.
+    """Returns the size of a file in bytes.
+
+    Keyword arguments:
+    f -- the file for which the size is to be determined
     """
     if isinstance(f, (six.string_types, six.text_type)):
         return os.path.getsize(f)
@@ -148,15 +216,37 @@ def file_size(f):
 
 
 class return_field(object):
-    """
-    Decorator that returns the given field of a json response.
+    """Decorator that returns the given field of a json response.
+
+    Public methods:
+    __init__ -- return_field constructor that sets the value of field
+    __call__ -- wraps a command so that only the value of field is returned
+
+    Instance variables:
+    field -- the desired response field for a command that will be wrapped
     """
     def __init__(self, field):
+        """Initializes a return_field object.
+
+        Keyword arguments:
+        field -- the response field to be returned for all function calls
+        """
         self.field = field
 
     def __call__(self, cmd):
+        """Wraps a command so that only a specified field is returned.
+
+        Keyword arguments:
+        cmd -- a command that is intended to be wrapped
+        """
         @wraps(cmd)
-        def wrapper(api, *args, **kwargs):
-            res = cmd(api, *args, **kwargs)
+        def wrapper(*args, **kwargs):
+            """Returns the specified field of the command invocation.
+
+            Keyword arguments:
+            args -- additional arguments to cmd
+            kwargs -- named additional arguments to cmd
+            """
+            res = cmd(*args, **kwargs)
             return res[self.field]
         return wrapper
---
+++ test/unit/test_utils.py
@@ -0,0 +1,209 @@
+"""Tox unit tests for utils.py.
+
+Classes:
+TestUtils -- defines a set of unit tests for untils.py
+"""
+
+import io
+import json
+import os
+import pickle
+import unittest
+
+import ipfsApi.utils as utils
+
+class TestUtils(unittest.TestCase):
+    """Contains unit tests for utils.py.
+
+    Public methods:
+    test_make_string_buffer -- tests utils.make_string_buffer()
+    test_encode_json -- tests utils.encode_json()
+    test_parse_json -- tests utils.parse_json()
+    test_make_json_buffer -- tests utils.make_json_buffer()
+    test_encode_pyobj -- tests utils.encode_pyobj()
+    test_parse_pyobj -- tests utils.parse_pyobj()
+    test_make_pyobj_buffer -- tests utils.make_pyobj_buffer()
+    test_guess_mimetype -- tests utils.guess_mimetype()
+    test_ls_dir -- tests utils.ls_dir()
+    test_clean_file_opened -- tests utils.clean_file() with a stringIO object
+    test_clean_file_unopened -- tests utils.clean_file() with a filepath
+    test_clean_files_single -- tests utils.clean_files() with a filepath
+    test_clean_files_list -- tests utils.clean_files() with a list of files
+    test_file_size -- tests utils.file_size()
+    test_return_field_init -- tests utils.return_field.__init__()
+    test_return_field_call -- tests utils.return_field.__call__()
+    """
+    def test_make_string_buffer(self):
+        """Tests utils.make_string_buffer()."""
+        raw = u'Mary had a little lamb'
+        buff = utils.make_string_buffer(raw)
+        self.assertEqual(hasattr(buff, 'read'), True)
+        self.assertEqual(hasattr(buff, 'write'), True)
+        self.assertEqual(buff.read(), raw)
+        # Closing buffer after test assertions.
+        buff.close()
+
+    def test_encode_json(self):
+        """Tests utils.encode_json()."""
+        data = {'key': 'value'}
+        self.assertEqual(utils.encode_json(data), json.dumps(data))
+
+    def test_parse_json(self):
+        """Tests utils.parse_json()."""
+        data = {'key': 'value'}
+        raw = json.dumps(data)
+        res = utils.parse_json(raw)
+        self.assertEqual(res['key'], 'value')
+
+    def test_make_json_buffer(self):
+        """Tests utils.make_json_buffer()."""
+        data = {'key': 'value'}
+        raw = json.dumps(data)
+        buff = utils.make_json_buffer(data)
+        self.assertEqual(hasattr(buff, 'read'), True)
+        self.assertEqual(hasattr(buff, 'write'), True)
+        self.assertEqual(buff.read(), raw)
+        # Closing buffer after test assertions.
+        buff.close()
+
+    def test_encode_pyobj(self):
+        """Tests utils.encode_pyobj().
+
+        In Python 2, data appears to be encoded differently based on the
+        context from which pickle.dumps() is called. For this reason we are
+        encoding and then decoding data to ensure that the decoded values are
+        equivalent after the original data has been serialized.
+        """
+        data = {'key': 'value'}
+        utils_res = pickle.loads(utils.encode_pyobj(data))
+        pickle_res = pickle.loads(pickle.dumps(data))
+        self.assertEqual(utils_res, pickle_res)
+
+    def test_parse_pyobj(self):
+        """Tests utils.parse_pyobj()."""
+        data = {'key': 'value'}
+        raw = pickle.dumps(data)
+        res = utils.parse_pyobj(raw)
+        self.assertEqual(res['key'], 'value')
+
+    def test_make_pyobj_buffer(self):
+        """Tests utils.make_pyobj_buffer().
+
+        In Python 2, data appears to be encoded differently based on the
+        context from which pickle.dumps() is called. For this reason we are
+        encoding and then decoding data to ensure that the decoded values are
+        equivalent after the original data has been serialized.
+        """
+        data = {'key': 'value'}
+        raw = pickle.dumps(data)
+        buff = utils.make_pyobj_buffer(data)
+        self.assertEqual(hasattr(buff, 'read'), True)
+        self.assertEqual(hasattr(buff, 'write'), True)
+        utils_res = pickle.loads(buff.read())
+        pickle_res = pickle.loads(raw)
+        self.assertEqual(utils_res, pickle_res)
+        # Closing buffer after test assertions.
+        buff.close()
+
+    def test_guess_mimetype(self):
+        """Tests utils.guess_mimetype().
+
+        Guesses the mimetype of the requirements.txt file
+        located in the project's root directory.
+        """
+        path = os.path.join(os.path.dirname(__file__),
+                            "..", "..", "requirements.txt")
+        self.assertEqual(utils.guess_mimetype(path),"text/plain")
+
+    def test_ls_dir(self):
+        """Tests utils.ls_dir()
+
+        This test is dependent on the contents of the directory 'fake_dir'
+        located in 'test/functional' not being modified.
+        """
+        path = os.path.join(os.path.dirname(__file__),
+                            "..", "functional", "fake_dir")
+        dirs = ['test2', 'test3']
+        files = ['fsdfgh', 'popoiopiu']
+        contents = (files, dirs)
+        self.assertEqual(utils.ls_dir(path), contents)
+
+    def test_clean_file_opened(self):
+        """Tests utils.clean_file() with a stringIO object."""
+        string_io = io.StringIO(u'Mary had a little lamb')
+        f, opened = utils.clean_file(string_io)
+        self.assertEqual(hasattr(f, 'read'), True)
+        self.assertEqual(opened, False)
+        # Closing stringIO after test assertions.
+        f.close()
+
+    def test_clean_file_unopened(self):
+        """Tests utils.clean_file() with a filepath.
+
+        This test relies on the openability of the file 'fsdfgh'
+        located in 'test/functional/fake_dir'.
+        """
+        path = os.path.join(os.path.dirname(__file__),
+                            "..", "functional", "fake_dir", "fsdfgh")
+        f, opened = utils.clean_file(path)
+        self.assertEqual(hasattr(f, 'read'), True)
+        self.assertEqual(opened, True)
+        # Closing file after test assertions.
+        f.close()
+
+    def test_clean_files_single(self):
+        """Tests utils.clean_files() with a singular filepath.
+
+        This test relies on the openability of the file 'fsdfgh'
+        located in 'test/functional/fake_dir'.
+        """
+        path = os.path.join(os.path.dirname(__file__),
+                            "..", "functional", "fake_dir", "fsdfgh")
+        gen = utils.clean_files(path)
+        for tup in gen:
+            self.assertEqual(hasattr(tup[0], 'read'), True)
+            self.assertEqual(tup[1], True)
+            # Closing file after test assertions.
+            tup[0].close()
+
+    def test_clean_files_list(self):
+        """Tests utils.clean_files() with a list of files/stringIO objects."""
+        path = os.path.join(os.path.dirname(__file__),
+                            "..", "functional", "fake_dir", "fsdfgh")
+        string_io = io.StringIO(u'Mary had a little lamb')
+        files = [path, string_io]
+        gen = utils.clean_files(files)
+        for i in range(0, 2):
+            tup = next(gen)
+            self.assertEqual(hasattr(tup[0], 'read'), True)
+            if i == 0:
+                self.assertEqual(tup[1], True)
+            else:
+                self.assertEqual(tup[1], False)
+            # Closing files/stringIO objects after test assertions.
+            tup[0].close()
+
+    def test_file_size(self):
+        """Tests utils.file_size().
+
+        This test relies on the content size of the file 'fsdfgh'
+        located in 'test/functional/fake_dir' not being modified.
+        """
+        path = os.path.join(os.path.dirname(__file__),
+                            "..", "functional", "fake_dir", "fsdfgh")
+        self.assertEqual(utils.file_size(path), 8)
+
+    def test_return_field_init(self):
+        """Tests utils.return_field.__init__()."""
+        return_field = utils.return_field('Hash')
+        self.assertEqual(return_field.field, 'Hash')
+
+    def test_return_field_call(self):
+        """Tests utils.return_field.__call__()."""
+        @utils.return_field('Hash')
+        def wrapper(string, *args, **kwargs):
+            resp = {'Hash':u'QmZfF6C9j4VtoCsTp4KSrhYH47QMd3DNXVZBKaxJdhaPab',
+                    'string': string}
+            return resp
+        self.assertEqual(wrapper('Mary had a little lamb'),
+                         u'QmZfF6C9j4VtoCsTp4KSrhYH47QMd3DNXVZBKaxJdhaPab')
