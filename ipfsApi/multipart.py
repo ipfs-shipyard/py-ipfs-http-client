914e1c04969b13c0b7db9f5c78a655c7f9b59d7a
Christopher Waldon <ckwaldon@us.ibm.com>
2016-07-12 17:06:01 -0400

    Document and test multipart.py
    License: MIT
    Signed-off-by: Christopher Waldon <christopher.waldon.dev@gmail.com>

--- ipfsApi/multipart.py
+++ ipfsApi/multipart.py
@@ -1,5 +1,22 @@
-"""
-Multipart/form-data encoded file streaming.
+"""HTTP Multipart/* encoded file streaming.
+
+Classes:
+BodyGenerator -- generates the body of an HTTP-encoded multipart request
+BufferedGenerator -- abstract class that generates the wrapper around HTTP
+                        multipart bodies
+FileStream -- generates the HTTP multipart request body for a group of files
+DirectoryStream -- generates the HTTP multipart request body for a directory
+TextStream -- generates the HTTP multipart request body for a chunk of text
+
+Functions:
+content_disposition -- returns a dict containing the MIME content-disposition
+                        header for a file
+content_type -- returns a dict with the content-type header for a file
+multipart_content_type -- creates a MIME multipart header with the given
+                            configuration
+stream_files -- gets a buffered generator for streaming files
+stream_directory -- gets a buffered generator for streaming directories
+stream_text -- gets a buffered generator for streaming text
 """
 from __future__ import absolute_import
 
@@ -25,15 +42,18 @@ default_chunk_size = 4096
 
 
 def content_disposition(fn, disptype='file'):
-    """
-    Returns a dict containing the MIME content-disposition header for a
-    file
+    """Returns a dict containing the MIME content-disposition header for a file.
 
+    Example:
     >>> content_disposition('example.txt')
     {'Content-Disposition': 'file; filename="example.txt"'}
 
     >>> content_disposition('example.txt', 'attachment')
     {'Content-Disposition': 'attachment; filename="example.txt"'}
+
+    Keyword arguments:
+    fn -- filename to retrieve the MIME content-disposition for
+    disptype -- the disposition type to use for the file
     """
     disp = '%s; filename="%s"' % (
         disptype,
@@ -43,10 +63,12 @@ def content_disposition(fn, disptype='file'):
 
 
 def content_type(fn):
-    """
-    Guesses the mimetype associated with a filename and returns a dict
-    containing the content-type header
+    """Returns a dict with the content-type header for a file.
 
+    Guesses the mimetype for a filename and returns a dict
+    containing the content-type header.
+
+    Example:
     >>> content_type('example.txt')
     {'Content-Type': 'text/plain'}
 
@@ -55,20 +77,29 @@ def content_type(fn):
 
     >>> content_type('example')
     {'Content-Type': 'application/octet-stream'}
+
+    Keyword Arguments:
+    fn -- filename to guess the content-type for
     """
     return {'Content-Type': utils.guess_mimetype(fn)}
 
 
 def multipart_content_type(boundary, subtype='mixed'):
-    """
-    Returns a dict containing a MIME multipart header with the given
-    boundary
+    """Creates a MIME multipart header with the given configuration.
 
+    Returns a dict containing a MIME multipart header with the given
+    boundary.
+
+    Example:
     >>> multipart_content_type('8K5rNKlLQVyreRNncxOTeg')
     {'Content-Type': 'multipart/mixed; boundary="8K5rNKlLQVyreRNncxOTeg"'}
 
     >>> multipart_content_type('8K5rNKlLQVyreRNncxOTeg', 'alt')
     {'Content-Type': 'multipart/alt; boundary="8K5rNKlLQVyreRNncxOTeg"'}
+
+    Keyword arguments:
+    boundary -- the boundary size to put in the header
+    subtype -- the MIME subtype to put in the header
     """
     ctype = 'multipart/%s; boundary="%s"' % (
         subtype,
@@ -78,11 +109,31 @@ def multipart_content_type(boundary, subtype='mixed'):
 
 
 class BodyGenerator(object):
-    """
-    Generators for multipart/form-data encoding.
-    """
-    def __init__(self, name, disptype='file', subtype='mixed', boundary=None):
+    """Generators for creating the body of a multipart/* HTTP request.
 
+    Instance variables:
+    boundary -- a separator for the body chunk being generated
+    headers -- the headers of the body chunk being generated
+
+    Public methods:
+    __init__ -- create a BodyGenerator
+    write_headers -- generator that yields HTTP headers for content
+    open -- generator that opens a body section for content
+    file_open -- generator that opens a file section for content
+    file_close -- generator that closes a file section for content
+    close -- generator that closes a body section for content
+    """
+
+    def __init__(self, name, disptype='file', subtype='mixed', boundary=None):
+        """Create a BodyGenerator.
+
+        Keyword arguments:
+        name -- the name of the file(s)/content being encoded
+        disptype -- the content-disposition of the content
+        subtype -- the HTTP multipart/<subtype> type of the content
+        boundary -- an identifier for the body being generated
+        """
+        # If the boundary is unspecified, make a random one
         if boundary is None:
             boundary = self._make_boundary()
         self.boundary = boundary
@@ -92,9 +143,21 @@ class BodyGenerator(object):
         self.headers = headers
 
     def _make_boundary(self):
+        """Returns a random hexadecimal string (UUID 4).
+
+        The HTTP multipart request body spec requires a boundary string to
+        separate different content chunks within a request, and this is
+        usually a random string. Using a UUID is an easy way to generate
+        a random string of appropriate length as this content separator.
+        """
         return uuid4().hex
 
     def _write_headers(self, headers):
+        """Generator function that yields the HTTP header for content.
+
+        Keyword arguments:
+        headers -- the dictionary of headers to yield
+        """
         if headers:
             for name in sorted(headers.keys()):
                 yield name
@@ -104,15 +167,27 @@ class BodyGenerator(object):
         yield CRLF
 
     def write_headers(self):
+        """Generator function that writes out the HTTP headers for content."""
         for c in self._write_headers(self.headers):
             yield c
 
     def open(self, **kwargs):
+        """Generator function that opens a body section for content.
+
+        Keyword arguments:
+        kwargs -- keyword arguments, unused
+        """
         yield b'--'
         yield self.boundary
         yield CRLF
 
     def file_open(self, fn):
+        """Generator function that opens a file section in multipart HTTP.
+
+        Keyword arguments:
+        fn -- filename for the file being opened and added to the HTTP
+                body
+        """
         yield b'--'
         yield self.boundary
         yield CRLF
@@ -122,9 +197,11 @@ class BodyGenerator(object):
             yield c
 
     def file_close(self):
+        """Generator function that ends a file section in HTTP encoding."""
         yield CRLF
 
     def close(self):
+        """Generator function that ends a content area in an HTTP body."""
         yield b'--'
         yield self.boundary
         yield b'--'
@@ -132,11 +209,35 @@ class BodyGenerator(object):
 
 
 class BufferedGenerator(object):
+    """Generator that encodes multipart/form-data.
+
+    An abstract class of buffered generator which encodes
+    multipart/form-data.
+
+    Instance variables:
+    chunk_size -- the maximum size of a generated chunk of a file
+    buf -- buffer containing the current file chunk
+    name -- the name of the file being chunked
+    envelope -- a BodyGenerator to wrap the chunked content
+    headers -- the HTTP multipart headers for the chunked content
+
+    Public methods:
+    __init__ -- generator that encodes multipart/form-data
+    file_chunks -- yields chunks of a file
+    gen_chunks -- generates byte chunks of a given size
+    body -- returns the body of the buffered file
+    close -- closes the multipart envelope
+    """
 
     def __init__(self, name, chunk_size=default_chunk_size):
-        """
+        """Generator that encodes multipart/form-data.
+
         An abstract class of buffered generator which encodes
         multipart/form-data.
+
+        Keyword arguments:
+        name -- the name of the file to encode
+        chunk_size -- the maxiumum size for any chunk in bytes
         """
         self.chunk_size = chunk_size
         self._internal = bytearray(chunk_size)
@@ -149,8 +250,11 @@ class BufferedGenerator(object):
         self.headers = self.envelope.headers
 
     def file_chunks(self, fp):
-        """
-        Yields chunks of a file.
+        """Yields chunks of a file.
+
+        Keyword arguments:
+        fp -- the file to break into chunks (must be an open file or have the
+                readinto method)
         """
         fsize = utils.file_size(fp)
         offset = 0
@@ -166,9 +270,13 @@ class BufferedGenerator(object):
                 offset += nb
 
     def gen_chunks(self, gen):
-        """
+        """Generates byte chunks of a given size.
+
         Takes a bytes generator and yields chunks of a maximum of [chunk_size]
         bytes.
+
+        Keyword arguments:
+        gen -- the bytes generator that produces the bytes
         """
         for data in gen:
             if not isinstance(data, six.binary_type):
@@ -185,28 +293,53 @@ class BufferedGenerator(object):
                     offset += nb
 
     def body(self, *args, **kwargs):
-        raise NotImplemented
+        """Returns the body of the buffered file.
+
+        Warning: this function is not implemented.
+
+        Keyword arguments:
+        args -- additional arguments, unused
+        kwargs -- additional keyword arguments, unused
+        """
+        raise NotImplementedError
 
     def close(self):
-        """
-        Closes the multipart envelope.
-        """
+        """Closes the multipart envelope."""
         for chunk in self.gen_chunks(self.envelope.close()):
             yield chunk
 
 
 class FileStream(BufferedGenerator):
+    """Generator that encodes multiples files into HTTP multipart.
+
+    A buffered generator that encodes an array of files as
+    multipart/form-data. This is a concrete implementation of
+    BufferedGenerator.
+
+    Instance variables:
+    files -- array of files to be encoded
+
+    Public methods:
+    __init__ -- creates a new FileStream
+    body -- generate the HTTP multipart-encoded body
+    """
 
     def __init__(self, files, chunk_size=default_chunk_size):
-        """
+        """Creates a new FileStream.
+
         A buffered generator that encodes an array of files as
         multipart/form-data.
+
+        Keyword arguments:
+        name -- the name of the file to encode
+        chunk_size -- the maxiumum size for any chunk in bytes
         """
         BufferedGenerator.__init__(self, 'files', chunk_size=chunk_size)
 
         self.files = utils.clean_files(files)
 
     def body(self):
+        """Returns the body of the buffered file."""
         for fp, need_close in self.files:
             try:
                 name = fp.name
@@ -225,15 +358,31 @@ class FileStream(BufferedGenerator):
 
 
 class DirectoryStream(BufferedGenerator):
+    """Generator that encodes a director into HTTP multipart.
+
+    A buffered generator that encodes a directory as
+    multipart/form-data.
+
+    Instance variables:
+    directory -- the directory being encoded
+    recursive -- whether or not to recursively encode directory contents
+    fnpattern -- a pattern to match filenames (if no match, file is excluded)
+
+    Public methods:
+    __init__ -- creates a new FileStream
+    body -- generate the HTTP multipart-encoded body
+    """
 
     def __init__(self,
                  directory,
                  recursive=False,
                  fnpattern='*',
                  chunk_size=default_chunk_size):
-        """
-        A buffered generator that encodes a directory as
-        multipart/form-data.
+        """A buffered generator that encodes a directory as multipart/form-data.
+
+        Keyword arguments:
+        directory -- the directory to encode
+        chunk_size -- the maximum size of a file chunk to use
         """
         BufferedGenerator.__init__(self, directory, chunk_size=chunk_size)
 
@@ -242,9 +391,15 @@ class DirectoryStream(BufferedGenerator):
         self.fnpattern = fnpattern
 
     def body(self, dirname=None, part=None):
-        """
+        """Encodes the directory as HTTP multipart.
+
         Recursively traverses a directory and generates the multipart encoded
         body.
+
+        Keyword arguments:
+        dirname -- name of directory to encode, defaults to self.dirname if
+                    None
+        part -- if not None, this is an inner content section
         """
         if part is None:
             # this is the outer envelope
@@ -294,16 +449,29 @@ class DirectoryStream(BufferedGenerator):
 
 
 class TextStream(BufferedGenerator):
+    """A buffered generator that encodes a string as multipart/form-data.
+
+    Instance variables:
+    text -- the text to stream
+
+    Public methods:
+    __init__ -- create a TextStream
+    body -- generator that yields the encoded body
+    """
 
     def __init__(self, text, chunk_size=default_chunk_size):
-        """
-        A buffered generator that encodes a string as multipart/form-data.
+        """A buffered generator that encodes a string as multipart/form-data.
+
+        Keyword arguments:
+        text -- the text to stream
+        chunk_size -- maximum size of a single data chunk
         """
         BufferedGenerator.__init__(self, 'text', chunk_size=chunk_size)
 
         self.text = text if isgenerator(text) else (text,)
 
     def body(self):
+        """Generator that yields the encoded body."""
         for chunk in self.gen_chunks(self.envelope.file_open(self.name)):
             yield chunk
         for chunk in self.gen_chunks(self.text):
@@ -315,9 +483,14 @@ class TextStream(BufferedGenerator):
 
 
 def stream_files(files, chunk_size=default_chunk_size):
-    """
+    """Gets a buffered generator for streaming files.
+
     Returns a buffered generator which encodes a file or list of files as
     multipart/form-data.  Also returns the corresponding headers.
+
+    Keyword arguments:
+    files -- the file(s) to stream
+    chunk_size -- maxiumum size of each stream chunk
     """
     stream = FileStream(files, chunk_size=chunk_size)
 
@@ -328,9 +501,16 @@ def stream_directory(directory,
                      recursive=False,
                      fnpattern='*',
                      chunk_size=default_chunk_size):
-    """
+    """Gets a buffered generator for streaming directories.
+
     Returns a buffered generator which encodes a directory as
     multipart/form-data.  Also returns the corresponding headers.
+
+    Keyword arguments:
+    directory -- the directory to stream
+    recursive -- boolean, True to stream all content recursively
+    fnpattern -- pattern of filenames to keep, functions like filter
+    chunk_size -- maximum size of each stream chunk
     """
     stream = DirectoryStream(directory,
                              recursive=recursive,
@@ -341,9 +521,14 @@ def stream_directory(directory,
 
 
 def stream_text(text, chunk_size=default_chunk_size):
-    """
+    """Gets a buffered generator for streaming text.
+
     Returns a buffered generator which encodes a string as multipart/form-data.
     Also retrns the corresponding headers.
+
+    Keyword arguments:
+    text -- the text to stream
+    chunk_size -- the maximum size of each stream chunk
     """
     stream = TextStream(text, chunk_size=chunk_size)
 
