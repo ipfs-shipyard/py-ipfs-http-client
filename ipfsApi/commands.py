801bc4ebba387660f40725a451712834d2136831
Christopher Waldon <ckwaldon@us.ibm.com>
2016-07-12 09:40:30 -0400

    Fully document and test commands.py
    License: MIT
    Signed-off-by: Christopher Waldon <christopher.waldon.dev@gmail.com>

--- ipfsApi/commands.py
+++ ipfsApi/commands.py
@@ -1,3 +1,12 @@
+"""Defines the skeleton of different command structures.
+
+Classes:
+Command -- A simple command that can make requests to a path.
+ArgCommand -- A Command subclass for commands with arguments.
+FileCommand -- A Command subclass for file-manipulation commands.
+DownloadCommand -- A Command subclass for file download commands.
+"""
+
 from __future__ import absolute_import
 
 import os
@@ -10,21 +19,71 @@ from .multipart import default_chunk_size
 
 
 class Command(object):
+    """Defines a command.
+
+    Public methods:
+    __init__ -- creates a Command that will make requests to a given path
+    request -- make a request to this command's path
+
+    Instance variables:
+    path -- the url path that this Command will make requests to
+    """
 
     def __init__(self, path):
+        """Creates a Command.
+
+        Keyword arguments:
+        path -- the url path that this Command makes requests to
+        """
         self.path = path
 
     def request(self, client, *args, **kwargs):
+        """Makes a request to the client with arguments.
+
+        Keyword arguments:
+        client -- the HTTP client to use for the request
+        args -- unused unnamed arguments
+        kwargs -- additional arguments to HTTP client's request
+        """
         return client.request(self.path, **kwargs)
 
 
 class ArgCommand(Command):
+    """Defines a command that takes arguments.
+
+    Subclass of Command.
+
+    Public methods:
+    __init__ -- extends Command constructor to also take a number of required
+                arguments
+    request -- makes a request to the ArgCommand's path with given arguments
+
+    Instance variables:
+    path -- the url path of that this command will send data to
+    argc -- the number of arguments required by this command
+    """
 
     def __init__(self, path, argc=None):
+        """Creates an ArgCommand.
+
+        Keyword arguments:
+        path -- the url path to which the command with send data
+        argc -- the number of arguments required by this command
+        """
         Command.__init__(self, path)
         self.argc = argc
 
     def request(self, client, *args, **kwargs):
+        """Makes a request to the client with arguments.
+
+        Can raise an InvalidArgument if the wrong number of arguments is
+        provided.
+
+        Keyword arguments:
+        client -- the HTTP client to use for the request
+        args -- the arguments to the HTTP client's request
+        kwargs -- additional arguments to HTTP client's request
+        """
         if self.argc and len(args) != self.argc:
             raise InvalidArguments("[%s] command requires %d arguments." % (
                 self.path, self.argc))
@@ -32,13 +91,33 @@ class ArgCommand(Command):
 
 
 class FileCommand(Command):
+    """Defines a command for manipulating files.
+
+    Subclass of Command.
+
+    Public methods:
+    request -- overrides Command's request to access a file or files
+    files -- adds file-like objects as a multipart request to IPFS
+    directory -- loads a directory recursively into IPFS
+
+    Instance variables:
+    path -- the path to make the file requests to
+    """
 
     def request(self, client, args, f, **kwargs):
-        """
-        Takes either a file object, a filename, an iterable of filenames, an
-        iterable of file objects, or a heterogeneous iterable of file objects
-        and filenames.  Can only take one directory at a time, which will be
+        """Makes a request for a file or files.
+
+        Can only take one directory at a time, which will be
         traversed (optionally recursive).
+
+        Keyword arguments:
+        client -- the http client to send requests to
+        args -- the arguments to the HTTP client's request
+        f -- a file object, a filename, an iterable of filenames, an
+                iterable of file objects, or a heterogeneous iterable of file
+                objects and filenames
+        kwargs -- additional arguments (include 'recursive' if recursively
+                copying a directory)
         """
         if kwargs.pop('recursive', False):
             return self.directory(client, args, f, recursive=True, **kwargs)
@@ -47,29 +126,67 @@ class FileCommand(Command):
         else:
             return self.files(client, args, f, **kwargs)
 
-    def files(self, client, args, files, chunk_size=default_chunk_size, **kwargs):
-        """
-        Adds file-like objects as a multipart request to IPFS.
+    def files(self, client, args, files,
+              chunk_size=default_chunk_size, **kwargs):
+        """Adds file-like objects as a multipart request to IPFS.
+
+        Keyword arguments:
+        client -- the http client to send requests to
+        args -- the arguments to the HTTP client's request
+        files -- the files being requested
+        chunk_size -- the size of the chunks to break the files into
+        kwargs -- additional arguments to HTTP client's request
         """
         body, headers = multipart.stream_files(files,
                                                chunk_size=chunk_size)
-        return client.request(self.path, args=args, data=body, headers=headers, **kwargs)
+        return client.request(self.path, args=args, data=body,
+                              headers=headers, **kwargs)
 
     def directory(self, client, args, dirname,
                   match='*', recursive=False,
                   chunk_size=default_chunk_size, **kwargs):
-        """
-        Loads a directory recursively into IPFS, files are matched against the
-        given pattern.
+        """Loads a directory recursively into IPFS.
+
+        Files are matched against the given pattern.
+
+        Keyword arguments:
+        client -- the http client to send requests to
+        args -- the arguments to the HTTP client's request
+        dirname -- the name of the directory being requested
+        match -- a pattern to match the files against
+        recursive -- boolean for whether to load contents recursively
+        chunk_size -- the size of the chunks to break the files into
+        kwargs -- additional arguments to HTTP client's request
         """
         body, headers = multipart.stream_directory(dirname,
                                                    fnpattern=match,
                                                    recursive=recursive,
                                                    chunk_size=chunk_size)
-        return client.request(self.path, args=args, data=body, headers=headers, **kwargs)
+        return client.request(self.path, args=args, data=body,
+                              headers=headers, **kwargs)
 
 
 class DownloadCommand(Command):
+    """Downloads requested files.
+
+    Subclass of Command
+
+    Public methods:
+    request -- make a request to this DownloadCommand's path to download a
+                given file
+
+    Instance variables:
+    path -- the url path to send requests to
+    """
 
     def request(self, client, *args, **kwargs):
+        """Requests a download from the HTTP Client.
+
+        See the HTTP client's doc for details of what to pass in.
+
+        Keyword arguments:
+        client -- the http client to send requests to
+        args -- the arguments to the HTTP client
+        kwargs -- additional arguments to the HTTP client
+        """
         return client.download(self.path, args=args, **kwargs)
--- test/unit/test_command.py
+++ test/unit/test_command.py
@@ -1,3 +1,14 @@
+"""Test commands.py.
+
+Classes:
+TestCommands -- test the functionality of the commands
+
+Functions:
+cmd_simple -- defines an endpoint for simple commands
+cmd_with_arg -- defines an endpoint for commands that have arguments
+cmd_with_file -- defines an endpoint for commands that handle files
+"""
+
 import unittest
 import json
 import six
@@ -9,9 +20,18 @@ from httmock import urlmatch, HTTMock
 import ipfsApi.commands
 import ipfsApi.exceptions
 
-
 @urlmatch(netloc='localhost:5001', path=r'.*/simple')
 def cmd_simple(url, request):
+    """Defines an endpoint for simple commands.
+
+    This endpoint will listen at http://localhost:5001/*/simple for incoming
+    requests and will always respond with a 200 status code and a Message of
+    "okay".
+
+    Keyword arguments:
+    url -- the url of the incoming request
+    request -- the request that is being responded to
+    """
     return {
         'status_code': 200,
         'content': json.dumps({
@@ -22,6 +42,16 @@ def cmd_simple(url, request):
 
 @urlmatch(netloc='localhost:5001', path=r'.*/arg')
 def cmd_with_arg(url, request):
+    """Defines an endpoint for commands that have arguments.
+
+    This endpoint will listen at http://localhost:5001/*/arg for incoming
+    requests and will always respond with a 200 status code, a Message of
+    "okay", and a list of Args.
+
+    Keyword arguments:
+    url -- the url of the incoming request
+    request -- the request that is being responded to
+    """
     qs = urlparse.parse_qs(url.query)
     return {
         'status_code': 200,
@@ -34,6 +64,16 @@ def cmd_with_arg(url, request):
 
 @urlmatch(netloc='localhost:5001', path=r'.*/file')
 def cmd_with_file(url, request):
+    """Defines an endpoint for commands that handle files.
+
+    This endpoint will listen at http://localhost:5001/*/file for incoming
+    requests and will always respond with a 200 status code, a Message of
+    "okay", and a file.
+
+    Keyword arguments:
+    url -- the url of the incoming request
+    request -- the request that is being responded to
+    """
     # request.body is a byte generator
     body = []
     for b in request.body:
@@ -53,7 +93,23 @@ def cmd_with_file(url, request):
 
 
 class TestCommands(unittest.TestCase):
+    """Test the functionality of the commands.py classes.
+
+    Public methods:
+    setUp -- create an HTTP client
+    test_simple_command -- test the Command class
+    test_arg_command_no_args -- test the ArgCommand class without a specific
+                                number of arguments
+    test_arg_command_with_args -- test the ArgCommand with a specific number
+                                of arguments
+    test_arg_command_wrong_num_args -- test that the ArgCommand class fails
+                                        when given the wrong number of
+                                        arguments
+    test_file_command_fd -- TODO 
+    """
+
     def setUp(self):
+        """Prepare tests."""
         self._client = ipfsApi.http.HTTPClient(
             'localhost',
             5001,
@@ -61,21 +117,40 @@ class TestCommands(unittest.TestCase):
             'json')
 
     def test_simple_command(self):
+        """Test the Command class."""
         with HTTMock(cmd_simple):
             cmd = ipfsApi.commands.Command('/simple')
             res = cmd.request(self._client)
             self.assertEquals(res['Message'], 'okay')
-    
-    def test_arg_command(self):
+
+    def test_arg_command_no_args(self):
+        """Test the ArgCommand class without a specific number of arguments."""
         with HTTMock(cmd_with_arg):
+            # test without arguments
             cmd = ipfsApi.commands.ArgCommand('/arg')
             res = cmd.request(self._client, 'arg1')
             self.assertEquals(res['Arg'][0], 'arg1')
-    
+
+    def test_arg_command_with_args(self):
+        """Test the ArgCommand with a specific number of arguments."""
+        with HTTMock(cmd_with_arg):
+            #test with arguments
+            cmd = ipfsApi.commands.ArgCommand('/arg', 2)
+            res = cmd.request(self._client, 'arg1', 'first')
+            self.assertEquals(res['Arg'], ['arg1', 'first'])
+
+    def test_arg_command_wrong_num_args(self):
+        """Test that the ArgCommand class fails when given the wrong number of arguments."""
+        with HTTMock(cmd_with_arg):
+            #test with wrong number of arguments
+            cmd = ipfsApi.commands.ArgCommand('/arg', 2)
+            self.assertRaises(ipfsApi.exceptions.InvalidArguments, cmd.request, self._client, 'arg1')
+
     def test_file_command_fd(self):
+        """Test a simple FileCommand."""
         data = 'content\ngoes\nhere'
         fd = StringIO(data)
         with HTTMock(cmd_with_file):
             cmd = ipfsApi.commands.FileCommand('/file')
-            res = cmd.request(self._client, fd)
+            res = cmd.request(self._client, (), fd)
             self.assertTrue(data in res['Body'])
