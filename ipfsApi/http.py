705e0c723d0c5d235a0f2d3197a8a0e703ea79fe
Ryan Soley <resoley@us.ibm.com>
2016-07-18 14:24:02 -0400

    Documented http.py and test_http.py and added additional tests to test_http.py.
    License: MIT
    Signed-off-by: Ryan Soley <resoley@us.ibm.com>

--- ipfsApi/http.py
+++ ipfsApi/http.py
@@ -1,6 +1,10 @@
-"""
-HTTP client for api requests.  This is pluggable into the IPFS Api client and
+"""HTTP client for api requests.
+
+This is pluggable into the IPFS Api client and
 can/will eventually be supplemented with an asynchronous version.
+
+Classes:
+Client -- A TCP client for interacting with an IPFS daemon
 """
 from __future__ import absolute_import
 
@@ -14,21 +18,49 @@ from . import encoding
 from .exceptions import ipfsApiError
 
 
-def pass_defaults(f):
-    """
-    Use instance default kwargs updated with those passed to function.
+def pass_defaults(func):
+    """Decorator that returns a function named wrapper.
+
+    When invoked, wrapper invokes func with default kwargs appended.
+
+    Keyword arguments:
+    func -- the function to append the default kwargs to
     """
     def wrapper(self, *args, **kwargs):
         merged = {}
         merged.update(self.defaults)
         merged.update(kwargs)
-        return f(self, *args, **merged)
+        return func(self, *args, **merged)
     return wrapper
 
 
 class HTTPClient(object):
+    """An HTTP client for interacting with the IPFS daemon.
+
+    Public methods:
+    __init__ -- initializes an instance of HTTPClient
+    request -- makes an HTTP request to the IPFS daemon
+    download -- makes a request to the IPFS daemon to download a file
+    session -- a context manager for this client's session
+
+    Instance variables:
+    host -- the host the IPFS daemon is running on
+    port -- the port the IPFS daemon is running on
+    base -- the path at which the api calls are to be sent
+    default_enc -- the default encoding of the HTTP client's response
+    defaults -- the default options to be handled by pass_defaults
+    """
 
     def __init__(self, host, port, base, default_enc, **defaults):
+        """Initializes an instance of HTTPClient.
+
+        Keyword arguments:
+        host -- the host the IPFS daemon is running on
+        port -- the port the IPFS daemon is running on
+        base -- the path at which the api calls are to be sent
+        default_enc -- the default encoding of the HTTP client's response
+        defaults -- the default options to be handled by pass_defaults
+        """
         self.host = host
         self.port = port
         if not re.match('^https?://', host.lower()):
@@ -50,7 +82,21 @@ class HTTPClient(object):
     def request(self, path,
                 args=[], files=[], opts={},
                 decoder=None, **kwargs):
+        """Makes an HTTP request to the IPFS daemon.
 
+        This function returns the contents of the HTTP response from the IPFS
+        daemon.
+
+        Keyword Arguments:
+        path -- the url path of this request
+        args -- a list of parameters to be sent with the HTTP request
+        files -- a file object, a filename, an iterable of filenames, an
+                    iterable of file objects, or a heterogeneous iterable of
+                    file objects and filenames
+        opts -- a dictonary of parameters to be sent with the HTTP request
+        decoder -- the encoding of the HTTP response, defaults to None
+        kwargs -- additional arguments, used to determine HTTP request method
+        """
         url = self.base + path
 
         params = []
@@ -107,9 +153,20 @@ class HTTPClient(object):
     def download(self, path, filepath=None,
                  args=[], opts={},
                  compress=True, **kwargs):
-        """
+        """Makes a request to the IPFS daemon to download a file.
+
         Downloads a file or files from IPFS into the current working
         directory, or the directory given by :filepath:.
+
+        Keyword Arguments:
+        path -- the url path of this request
+        filepath -- the local path to where IPFS will download the files,
+                    current working directory if None, defaults to None
+        args -- a list of parameters to be sent with the HTTP request
+        opts -- a dictonary of parameters to be sent with the HTTP request
+        compress -- a flag that when true indicates that the response file
+                    should be downloaded as a tar, defaults to True
+        kwargs -- additional arguments
         """
         url = self.base + path
         wd = filepath or '.'
@@ -144,6 +201,11 @@ class HTTPClient(object):
 
     @contextlib.contextmanager
     def session(self):
+        """A context manager for this client's session.
+
+        This function closes the current session when this client goes out of
+        scope.
+        """
         self._session = requests.session()
         yield
         self._session.close()
--- test/unit/test_http.py
+++ test/unit/test_http.py
@@ -1,6 +1,17 @@
+"""Test cases for http.py.
+
+These tests are designed to mock http responses from the IPFS daemon. They
+are used to determine if the functions in http.py are operating correctly.
+
+Classes:
+TestHttp -- A TCP client for interacting with an IPFS daemon
+"""
+
 import unittest
 import json
 import requests
+import tarfile
+import os
 from httmock import urlmatch, HTTMock
 
 import ipfsApi.http
@@ -9,6 +20,16 @@ import ipfsApi.exceptions
 
 @urlmatch(netloc='localhost:5001', path=r'.*/okay')
 def return_okay(url, request):
+    """Defines an endpoint for successful http requests.
+
+    This endpoint will listen at http://localhost:5001/*/okay for incoming
+    requests and will always respond with a 200 status code and a Message of
+    "okay".
+
+    Keyword arguments:
+    url -- the url of the incoming request
+    request -- the request that is being responded to
+    """
     return {
         'status_code': 200,
         'content': 'okay'.encode('utf-8'),
@@ -17,6 +38,16 @@ def return_okay(url, request):
 
 @urlmatch(netloc='localhost:5001', path=r'.*/fail')
 def return_fail(url, request):
+    """Defines an endpoint for failed http requests.
+
+    This endpoint will listen at http://localhost:5001/*/fail for incoming
+    requests and will always respond with a 500 status code and a Message of
+    "fail".
+
+    Keyword arguments:
+    url -- the url of the incoming request
+    request -- the request that is being responded to
+    """
     return {
         'status_code': 500,
         'content': 'fail'.encode('utf-8'),
@@ -25,6 +56,16 @@ def return_fail(url, request):
 
 @urlmatch(netloc='localhost:5001', path=r'.*/apiokay')
 def api_okay(url, request):
+    """Defines an endpoint for successful api requests.
+
+    This endpoint will listen at http://localhost:5001/*/apiokay for incoming
+    requests and will always respond with a 200 status code and a json encoded
+    Message of "okay".
+
+    Keyword arguments:
+    url -- the url of the incoming request
+    request -- the request that is being responded to
+    """
     return {
         'status_code': 200,
         'content': json.dumps({
@@ -34,6 +75,16 @@ def api_okay(url, request):
 
 @urlmatch(netloc='localhost:5001', path=r'.*/apifail')
 def api_fail(url, request):
+    """Defines an endpoint for failed api requests.
+
+    This endpoint will listen at http://localhost:5001/*/apifail for incoming
+    requests and will always respond with a 500 status code and a json encoded
+    Message of "Someone set us up the bomb".
+
+    Keyword arguments:
+    url -- the url of the incoming request
+    request -- the request that is being responded to
+    """
     return {
         'status_code': 500,
         'content': json.dumps({
@@ -41,8 +92,47 @@ def api_fail(url, request):
     }
 
 
+@urlmatch(netloc='localhost:5001', path=r'.*/cat')
+def api_cat(url, request):
+    """Defines an endpoint for a request to cat a file.
+
+    This endpoint will listen at http://localhost:5001/*/cat for incoming
+    requests and will always respond with a 200 status code and a json encoded
+    Message of "do not parse".
+
+    Keyword arguments:
+    url -- the url of the incoming request
+    request -- the request that is being responded to
+    """
+    return {
+        'status_code': 200,
+        'content': json.dumps({
+            'Message': 'do not parse'}).encode('utf-8')
+    }
+
+
 class TestHttp(unittest.TestCase):
+    """A series of tests to test the functionality of http.py.
+
+    Public methods:
+    setUp -- creates an instance of HTTPClient to test against
+    test_successful_request -- tests that a successful http request returns the
+                                proper message
+    test_generic_failure -- tests that a failed http request raises an HTTPError
+    test_api_failure -- tests that an api failure raises an ispfApiError
+    test_stream -- tests that the stream flag being set returns the raw response
+    test_cat -- tests that paths ending in /cat are not parsed
+    test_default_decoder -- tests that the default encoding is set to json
+    test_explicit_decoder -- tests that an explicit decoder is handled correctly
+    test_unsupported_decoder -- tests that unsupported encodings raise an
+                                EncodingException
+    test_failed_decoder -- tests that a failed encoding parse returns response
+                            text
+    test_failed_download -- tests that a failed download raises an HTTPError
+    test_session -- tests that a session is established and then closed
+    """
     def setUp(self):
+        """Creates an instance of HTTPClient to test against."""
         self.client = ipfsApi.http.HTTPClient(
             'localhost',
             5001,
@@ -50,28 +140,76 @@ class TestHttp(unittest.TestCase):
             'json')
 
     def test_successful_request(self):
+        """Tests that a successful http request returns the proper message."""
         with HTTMock(return_okay):
             res = self.client.request('/okay')
             self.assertEqual(res, 'okay')
 
     def test_generic_failure(self):
+        """Tests that a failed http request raises an HTTPError."""
         with HTTMock(return_fail):
             self.assertRaises(requests.HTTPError,
                               self.client.request, '/fail')
 
     def test_api_failure(self):
+        """Tests that an api failure raises an ispfApiError."""
         with HTTMock(api_fail):
             self.assertRaises(ipfsApi.exceptions.ipfsApiError,
                               self.client.request, '/apifail')
 
-    def test_session(self):
+    def test_stream(self):
+        """Tests that the stream flag being set returns the raw response."""
         with HTTMock(return_okay):
-            with self.client.session():
-                res = self.client.request('/okay')
-                self.assertEqual(res, 'okay')
+            res = self.client.request('/okay', stream=True)
+            self.assertEqual(res.read(4), b'okay')
+
+    def test_cat(self):
+        """Tests that paths ending in /cat are not parsed."""
+        with HTTMock(api_cat):
+            res = self.client.request('/cat')
+            self.assertEquals(res, '{"Message": "do not parse"}')
+
+    def test_default_decoder(self):
+        """Tests that the default encoding is set to json."""
+        with HTTMock(api_okay):
+            res = self.client.request('/apiokay')
+            self.assertEquals(res['Message'], 'okay')
 
     def test_explicit_decoder(self):
+        """Tests that an explicit decoder is handled correctly."""
         with HTTMock(api_okay):
             res = self.client.request('/apiokay',
                                       decoder='json')
             self.assertEquals(res['Message'], 'okay')
+
+    def test_unsupported_decoder(self):
+        """Tests that unsupported encodings raise an EncodingException."""
+        with HTTMock(api_fail):
+            self.assertRaises(ipfsApi.exceptions.EncodingException,
+                              self.client.request, '/apifail', decoder='xyz')
+
+    def test_failed_decoder(self):
+        """Tests that a failed encoding parse returns response text."""
+        with HTTMock(return_okay):
+            res = self.client.request('/okay', decoder='json')
+            self.assertEquals(res, 'okay')
+
+    """TODO: Test successful download
+    Need to determine correct way to mock an http request that returns a tar
+    file. tarfile.open expects the tar to be in the form of an octal escaped
+    string, but internal functionality keeps resulting in hexidecimal.
+    """
+
+    def test_failed_download(self):
+        """Tests that a failed download raises an HTTPError."""
+        with HTTMock(return_fail):
+            self.assertRaises(requests.HTTPError,
+                              self.client.download, '/fail')
+
+    def test_session(self):
+        """Tests that a session is established and then closed."""
+        with HTTMock(return_okay):
+            with self.client.session():
+                res = self.client.request('/okay')
+                self.assertEqual(res, 'okay')
+            self.assertEqual(self.client._session, None)
