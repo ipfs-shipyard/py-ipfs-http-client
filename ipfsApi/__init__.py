1ba80e6129d7cbc1f671eb216c7b30e0ea3ef16c
Alexander Schlarb <alexander@xmine128.tk>
2016-08-29 00:03:41 +0200

    More formally define what we consider to be part of the stable API by only importing those parts into `__init__.py`

--- ipfsApi/__init__.py
+++ ipfsApi/__init__.py
@@ -1,5 +1,11 @@
 from __future__ import absolute_import
 
-from .client import *
-
 __version__ = '0.2.3'
+
+###########################
+# Import stable API parts #
+###########################
+from . import exceptions
+
+from .client import DEFAULT_HOST, DEFAULT_PORT, DEFAULT_BASE
+from .client import Client
