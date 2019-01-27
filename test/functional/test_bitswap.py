168d3a1c64896ea6358f91d3388ecc23330a6319
Alexander Schlarb <alexander@ninetailed.ninja>
2019-01-27 19:39:52 +0100

    Drop `c.bitswap.unwant` since support for it has been permanently dropped by go-IPFS
    Source: https://github.com/ipfs/go-ipfs/pull/5308

--- test/functional/test_bitswap.py
+++ test/functional/test_bitswap.py
@@ -11,12 +11,3 @@ def test_stat(client):
 	result = client.bitswap.stat()
 	assert type(result) is dict
 	assert "Wantlist" in result
-
-
-def test_unwant(client):
-	"""
-	Cannot ensure what is present in the wantlist prior to execution, so just ensure
-	something comes back.
-	"""
-	result = client.bitswap.unwant(key="QmZTR5bcpQD7cFgTorqxZDYaew1Wqgfbd2ud9QqGPAkK2V")
-	assert result is not None
