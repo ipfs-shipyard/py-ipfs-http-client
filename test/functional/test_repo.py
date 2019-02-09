3d71484de152fc71c154b9afb6236149e580df6c
Nathan <lordcirth@gmail.com>
2019-02-09 01:33:07 +0000

    test for gc(return_result=False)

--- test/functional/test_repo.py
+++ test/functional/test_repo.py
@@ -24,3 +24,18 @@ def test_gc(client):
 	assert orig_objs > cur_objs
 	keys = [el["Key"]["/"] for el in gc]
 	assert garbage in keys
+
+
+def test_gc_no_result(client):
+	# Add and unpin an object to be garbage collected
+	garbage = client.add_str("Test String")
+	client.pin.rm(garbage)
+
+	# Collect the garbage object with object count before and after
+	orig_objs = client.repo.stat()["NumObjects"]
+	gc = client.repo.gc(return_result=False)
+	cur_objs = client.repo.stat()["NumObjects"]
+
+	# Verify the garbage object was collected
+	assert orig_objs > cur_objs
+	assert gc is None
