6b1cfa1851eeac38e79272be062d2b75bc4351a6
redfish <redfish@galactica.pw>
2019-03-03 13:46:13 -0500

    test: name: test in offline mode for speed

--- test/functional/test_name.py
+++ test/functional/test_name.py
@@ -3,9 +3,6 @@
 import pytest
 
 
-TIMEOUT = 300
-
-
 def get_key(client, key_name):
 	keys = client.key.list()["Keys"]
 	for k in keys:
@@ -29,8 +26,8 @@ def hash_to_ipns_path(h):
 
 
 class Resources(object):
-	def __init__(self, client):
-		self.client = client
+	def __init__(self, offline_client):
+		self.client = offline_client
 
 
 	def __enter__(self):
@@ -55,18 +52,18 @@ class PublishedMapping(object):
 
 
 @pytest.fixture(scope="module")
-def resources(module_client):
-	with Resources(module_client) as resources:
+def resources(module_offline_client):
+	with Resources(module_offline_client) as resources:
 		yield resources
 
 
 @pytest.fixture(scope="module")
-def published_mapping(module_client, resources):
+def published_mapping(module_offline_client, resources):
 	# we're not testing publish here, pass whatever args we want
-	resp = module_client.name.publish(resources.msg3,
-	                                  key=resources.key_test2["Name"],
-	                                  resolve=False, lifetime="5m", ttl="5m",
-	                                  allow_offline=True, timeout=TIMEOUT)
+	resp = module_offline_client.name.publish(
+		resources.msg3,
+		key=resources.key_test2["Name"], resolve=False,
+		lifetime="5m", ttl="5m", allow_offline=True)
 	return PublishedMapping(resp["Name"], resp["Value"])
 
 
@@ -74,66 +71,68 @@ def check_resolve(resp, path):
 	assert resp["Path"] == path
 
 
-def check_publish(client, response_path, resolved_path, key, resp):
+def check_publish(offline_client, response_path, resolved_path, key, resp):
 
 	name = resp["Name"]
 	assert name == key["Id"]
 	assert resp["Value"] == response_path
 
 	# we're not testing resolve here, pass whatever args we want
-	resolve_resp = client.name.resolve(name, recursive=True,
-	                                   dht_record_count=0, dht_timeout="1s",
-	                                   timeout=TIMEOUT)
+	resolve_resp = offline_client.name.resolve(
+		name,
+		recursive=True, dht_record_count=0, dht_timeout="1s",
+		offline=True)
 	check_resolve(resolve_resp, resolved_path)
 
 
-def test_publish_self(client, resources):
-	resp = client.name.publish(resources.msg1,
-	                           allow_offline=True, timeout=TIMEOUT)
-	check_publish(client, resources.msg1, resources.msg1,
+def test_publish_self(offline_client, resources):
+	resp = offline_client.name.publish(resources.msg1, allow_offline=True)
+	check_publish(offline_client, resources.msg1, resources.msg1,
 	              resources.key_self, resp)
 
 
-def test_publish_params(client, resources):
-	resp = client.name.publish(resources.msg1, lifetime="25h", ttl="1m",
-	                           allow_offline=True, timeout=TIMEOUT)
-	check_publish(client, resources.msg1, resources.msg1,
+def test_publish_params(offline_client, resources):
+	resp = offline_client.name.publish(resources.msg1,
+	                                   lifetime="25h", ttl="1m",
+	                                   allow_offline=True)
+	check_publish(offline_client, resources.msg1, resources.msg1,
 	              resources.key_self, resp)
 
 
-def test_publish_key(client, resources):
-	resp = client.name.publish(resources.msg2,
-	                           key=resources.key_test1["Name"],
-	                           allow_offline=True, timeout=TIMEOUT)
-	check_publish(client, resources.msg2, resources.msg2,
+def test_publish_key(offline_client, resources):
+	resp = offline_client.name.publish(
+		resources.msg2,
+		key=resources.key_test1["Name"], allow_offline=True)
+	check_publish(offline_client, resources.msg2, resources.msg2,
 	              resources.key_test1, resp)
 
 
-def test_publish_indirect(client, resources, published_mapping):
+def test_publish_indirect(offline_client, resources, published_mapping):
 	path = hash_to_ipns_path(published_mapping.name)
-	resp = client.name.publish(path, resolve=True,
-	                           allow_offline=True, timeout=TIMEOUT)
-	check_publish(client, path, published_mapping.path,
+	resp = offline_client.name.publish(path,
+	                                   resolve=True, allow_offline=True)
+	check_publish(offline_client, path, published_mapping.path,
 	              resources.key_self, resp)
 
 
-def test_resolve(client, published_mapping):
-	check_resolve(client.name.resolve(published_mapping.name, timeout=TIMEOUT),
+def test_resolve(offline_client, published_mapping):
+	check_resolve(offline_client.name.resolve(published_mapping.name),
 	              published_mapping.path)
 
 
-def test_resolve_recursive(client, published_mapping):
+def test_resolve_recursive(offline_client, published_mapping):
 	inner_path = hash_to_ipns_path(published_mapping.name)
-	res = client.name.publish(inner_path, resolve=False,
-	                          allow_offline=True, timeout=TIMEOUT)
+	res = offline_client.name.publish(inner_path,
+	                                  resolve=False, allow_offline=True)
 	outer_path = res["Name"]
 
-	resp = client.name.resolve(outer_path, recursive=True, timeout=TIMEOUT)
+	resp = offline_client.name.resolve(outer_path, recursive=True)
 	check_resolve(resp, published_mapping.path)
 
 
-def test_resolve_params(client, published_mapping):
-	resp = client.name.resolve(published_mapping.name,  nocache=True,
-	                           dht_record_count=1, dht_timeout="180s",
-	                           timeout=TIMEOUT)
+def test_resolve_params(offline_client, published_mapping):
+	resp = offline_client.name.resolve(
+		published_mapping.name,
+		nocache=True, dht_record_count=1, dht_timeout="180s",
+		offline=True)
 	check_resolve(resp, published_mapping.path)
