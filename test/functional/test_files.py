ipfs-shipyard/py-ipfs-http-client

commit 52df768c4efdd0b121619e02780bbd2502b66133

Make it part of the function declaration whether the given function returns only a single item or not
Previously the code would rely on a heuristic where it would return only a
single item if the daemon only returned one item, a list otherwise. This could
have unexpected results in some cases (#133).

ipfshttpclient/client/files.py
@@ -11,7 +11,8 @@ class Section(base.SectionBase):
 	Functions used to manage files in IPFS's virtual “Mutable File System” (MFS)
 	file storage space.
 	"""
-
+	
+	@base.returns_single_item
 	def cp(self, source, dest, **kwargs):
 		"""Copies files within the MFS.
 
@@ -49,6 +50,7 @@ class Section(base.SectionBase):
 	#TODO: Add `flush(path="/")`
 
 
+	@base.returns_single_item
 	def ls(self, path, **kwargs):
 		"""Lists contents of a directory in the MFS.
 
@@ -70,15 +72,15 @@ class Section(base.SectionBase):
 		"""
 		args = (path,)
 		return self._client.request('/files/ls', args, decoder='json', **kwargs)
-
-
+	
+	
+	@base.returns_no_item
 	def mkdir(self, path, parents=False, **kwargs):
 		"""Creates a directory within the MFS.
 
 		.. code-block:: python
 
 			>>> client.files.mkdir("/test")
-			b''
 
 		Parameters
 		----------
@@ -92,15 +94,15 @@ class Section(base.SectionBase):
 
 		args = (path,)
 		return self._client.request('/files/mkdir', args, **kwargs)
-
-
+	
+	
+	@base.returns_no_item
 	def mv(self, source, dest, **kwargs):
 		"""Moves files and directories within the MFS.
 
 		.. code-block:: python
 
 			>>> client.files.mv("/test/file", "/bla/file")
-			b''
 
 		Parameters
 		----------
@@ -111,8 +113,8 @@ class Section(base.SectionBase):
 		"""
 		args = (source, dest)
 		return self._client.request('/files/mv', args, **kwargs)
-
-
+	
+	
 	def read(self, path, offset=0, count=None, **kwargs):
 		"""Reads a file stored in the MFS.
 
@@ -141,8 +143,9 @@ class Section(base.SectionBase):
 
 		args = (path,)
 		return self._client.request('/files/read', args, **kwargs)
-
-
+	
+	
+	@base.returns_no_item
 	def rm(self, path, recursive=False, **kwargs):
 		"""Removes a file from the MFS.
 
@@ -161,8 +164,9 @@ class Section(base.SectionBase):
 
 		args = (path,)
 		return self._client.request('/files/rm', args, **kwargs)
-
-
+	
+	
+	@base.returns_single_item
 	def stat(self, path, **kwargs):
 		"""Returns basic ``stat`` information for an MFS file
 		(including its hash).
@@ -184,8 +188,9 @@ class Section(base.SectionBase):
 		"""
 		args = (path,)
 		return self._client.request('/files/stat', args, decoder='json', **kwargs)
-
-
+	
+	
+	@base.returns_no_item
 	def write(self, path, file, offset=0, create=False, truncate=False, count=None, **kwargs):
 		"""Writes to a mutable file in the MFS.
 
@@ -221,8 +226,8 @@ class Section(base.SectionBase):
 
 class Base(base.ClientBase):
 	files = base.SectionProperty(Section)
-
-
+	
+	
 	def add(self, file, *files, **kwargs):
 		"""Add a file, or directory of files to IPFS.
 
@@ -262,7 +267,9 @@ class Base(base.ClientBase):
 
 		Returns
 		-------
-			dict: File name and hash of the added file node
+			Union[dict, list] : File name and hash of the added file node, will
+			                    return a list of one or more items unless only
+			                    a single file was given
 		"""
 		#PY2: No support for kw-only parameters after glob parameters
 		recursive = kwargs.pop("recursive", False)
@@ -281,12 +288,18 @@ class Base(base.ClientBase):
 			"Use `client.add(name1, name2, …)` to add several items"
 		multiple = (len(files) > 0)
 		to_send  = ((file,) + files) if multiple else file
-		body, headers = multipart.stream_filesystem_node(
+		body, headers, is_dir = multipart.stream_filesystem_node(
 			to_send, recursive, pattern, self.chunk_size
 		)
-		return self._client.request('/add', decoder='json', data=body, headers=headers, **kwargs)
-
-
+		
+		resp = self._client.request('/add', decoder='json', data=body, headers=headers, **kwargs)
+		if not multiple and not is_dir and not kwargs["opts"]["wrap-with-directory"]:
+			assert len(resp) == 1
+			return resp[0]
+		return resp
+	
+	
+	@base.returns_single_item
 	def file_ls(self, multihash, **kwargs):
 		"""Lists directory contents for Unix filesystem objects.
 
@@ -334,8 +347,8 @@ class Base(base.ClientBase):
 		"""
 		args = (multihash,)
 		return self._client.request('/file/ls', args, decoder='json', **kwargs)
-
-
+	
+	
 	def get(self, cid, **kwargs):
 		"""Downloads a file, or directory of files from IPFS.
 
@@ -348,8 +361,8 @@ class Base(base.ClientBase):
 		"""
 		args = (str(cid),)
 		return self._client.download('/get', args, **kwargs)
-
-
+	
+	
 	def cat(self, cid, offset=0, length=-1, **kwargs):
 		r"""Retrieves the contents of a file identified by hash.
 
@@ -383,8 +396,9 @@ class Base(base.ClientBase):
 			opts['length'] = length
 		kwargs.setdefault('opts', opts)
 		return self._client.request('/cat', args, **kwargs)
-
-
+	
+	
+	@base.returns_single_item
 	def ls(self, cid, **kwargs):
 		"""Returns a list of objects linked to by the given hash.
 

ipfshttpclient/client/__init__.py
@@ -116,6 +116,7 @@ class Client(files.Base, miscellaneous.Base):
 	###########
 
 	@utils.return_field('Hash')
+	@base.returns_single_item
 	def add_bytes(self, data, **kwargs):
 		"""Adds a set of bytes as a file to IPFS.
 
@@ -140,6 +141,7 @@ class Client(files.Base, miscellaneous.Base):
 		                            data=body, headers=headers, **kwargs)
 
 	@utils.return_field('Hash')
+	@base.returns_single_item
 	def add_str(self, string, **kwargs):
 		"""Adds a Python string as a file to IPFS.
 
@@ -181,7 +183,9 @@ class Client(files.Base, miscellaneous.Base):
 			str : Hash of the added IPFS object
 		"""
 		return self.add_bytes(encoding.Json().encode(json_obj), **kwargs)
-
+	
+	
+	@base.returns_single_item
 	def get_json(self, cid, **kwargs):
 		"""Loads a json object from IPFS.
 

ipfshttpclient/client/base.py
@@ -1,9 +1,45 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+import functools
+
+import six
 
 from . import DEFAULT_HOST, DEFAULT_PORT, DEFAULT_BASE
 
 from .. import multipart, http
+
+
+def returns_single_item(func):
+	@functools.wraps(func)
+	def wrapper(*args, **kwargs):
+		result = func(*args, **kwargs)
+		if isinstance(result, list):
+			if len(result) != 1:
+				print(result)
+			assert len(result) == 1, ("Called IPFS HTTP-Client function should "
+			                          "only ever return one item")
+			return result[0]
+		assert kwargs.get("stream", False), ("Called IPFS HTTP-Client function "
+		                                     "should only ever return a list, "
+		                                     "when not streaming a response")
+		return result
+	return wrapper
+
+
+def returns_no_item(func):
+	@functools.wraps(func)
+	def wrapper(*args, **kwargs):
+		result = func(*args, **kwargs)
+		if isinstance(result, (list, six.binary_type)):
+			assert len(result) == 0, ("Called IPFS HTTP-Client function should "
+			                          "never return an item")
+			return
+		assert kwargs.get("stream", False), ("Called IPFS HTTP-Client function "
+		                                     "should only ever return a list "
+		                                     "or bytes, when not streaming a "
+		                                     "response")
+		return result
+	return wrapper
 
 
 class SectionProperty(object):

ipfshttpclient/client/bitswap.py
@@ -5,6 +5,7 @@ from . import base
 
 
 class Section(base.SectionBase):
+	@base.returns_single_item
 	def wantlist(self, peer=None, **kwargs):
 		"""Returns blocks currently on the bitswap wantlist.
 		
@@ -30,6 +31,7 @@ class Section(base.SectionBase):
 		return self._client.request('/bitswap/wantlist', args, decoder='json', **kwargs)
 	
 	
+	@base.returns_single_item
 	def stat(self, **kwargs):
 		"""Returns some diagnostic information from the bitswap agent.
 		

ipfshttpclient/client/block.py
@@ -30,8 +30,9 @@ class Section(base.SectionBase):
 		"""
 		args = (str(cid),)
 		return self._client.request('/block/get', args, **kwargs)
-
-
+	
+	
+	@base.returns_single_item
 	def put(self, file, **kwargs):
 		"""Stores the contents of the given file object as an IPFS block.
 
@@ -55,8 +56,9 @@ class Section(base.SectionBase):
 		body, headers = multipart.stream_files(file, self.chunk_size)
 		return self._client.request('/block/put', decoder='json', data=body,
 		                            headers=headers, **kwargs)
-
-
+	
+	
+	@base.returns_single_item
 	def stat(self, cid, **kwargs):
 		"""Returns a dict with the size of the block with the given hash.
 

ipfshttpclient/client/bootstrap.py
@@ -5,6 +5,7 @@ from . import base
 
 
 class Section(base.SectionBase):
+	@base.returns_single_item
 	def add(self, peer, *peers, **kwargs):
 		"""Adds peers to the bootstrap list.
 
@@ -19,8 +20,9 @@ class Section(base.SectionBase):
 		"""
 		args = (peer,) + peers
 		return self._client.request('/bootstrap/add', args, decoder='json', **kwargs)
-
-
+	
+	
+	@base.returns_single_item
 	def list(self, **kwargs):
 		"""Returns the addresses of peers used during initial discovery of the
 		IPFS network.
@@ -42,8 +44,9 @@ class Section(base.SectionBase):
 			dict : List of known bootstrap peers
 		"""
 		return self._client.request('/bootstrap', decoder='json', **kwargs)
-
-
+	
+	
+	@base.returns_single_item
 	def rm(self, peer, *peers, **kwargs):
 		"""Removes peers from the bootstrap list.
 

ipfshttpclient/client/config.py
@@ -5,6 +5,7 @@ from . import base
 
 
 class Section(base.SectionBase):
+	@base.returns_single_item
 	def get(self, **kwargs):
 		#TODO: Support the optional `key` parameter
 		"""Returns the current used server configuration.
@@ -29,8 +30,9 @@ class Section(base.SectionBase):
 			dict : The entire IPFS daemon configuration
 		"""
 		return self._client.request('/config/show', decoder='json', **kwargs)
-
-
+	
+	
+	@base.returns_single_item
 	def replace(self, config, **kwargs):
 		"""Replaces the existing config with a user-defined config.
 
@@ -38,8 +40,9 @@ class Section(base.SectionBase):
 		operation can't be undone.
 		"""
 		return self._client.request('/config/replace', (config,), decoder='json', **kwargs)
-
-
+	
+	
+	@base.returns_single_item
 	def set(self, key, value=None, **kwargs):
 		"""Add or replace a configuration value.
 

ipfshttpclient/client/dht.py
@@ -7,6 +7,7 @@ from .. import exceptions
 
 
 class Section(base.SectionBase):
+	@base.returns_single_item
 	def findpeer(self, peer_id, *peer_ids, **kwargs):
 		"""Queries the DHT for all of the associated multiaddresses.
 
@@ -80,8 +81,9 @@ class Section(base.SectionBase):
 		"""
 		args = (str(cid),) + tuple(str(c) for c in cids)
 		return self._client.request('/dht/findprovs', args, decoder='json', **kwargs)
-
-
+	
+	
+	@base.returns_single_item
 	def get(self, key, *keys, **kwargs):
 		"""Queries the DHT for its best value related to given key.
 

commit cc14a1b7b8c56559fcb27fa3a51978bc42dff8bb

files: fix return attribute in cp to be void
Fixes assert failure introduced in 52df768c.
Added test that would have caught the problem.

ipfshttpclient/client/files.py
@@ -12,7 +12,7 @@ class Section(base.SectionBase):
 	file storage space.
 	"""
 	
-	@base.returns_single_item
+	@base.returns_no_item
 	def cp(self, source, dest, **kwargs):
 		"""Copies files within the MFS.
 

test/functional/test_files.py
@@ -297,6 +297,18 @@ TEST_MFS_FILES = {
 TEST_MFS_DIRECTORY = "/test_dir"
 
 
+def test_mfs_file_cp_rm(client, cleanup_pins):
+	res = client.add(FAKE_FILE1_PATH)
+	h = res["Hash"]
+
+	mfs_path = "/" + TEST_MFS_DIRECTORY + "file1"
+	res = client.files.cp("/ipfs/" + h, mfs_path)
+	assert res is None
+
+	res = client.files.rm(mfs_path)
+	assert res is None
+
+
 def test_mfs_file_write_stat_read_delete(client):
 	for filename, desc in TEST_MFS_FILES.items():
 		filepath = "/" + filename