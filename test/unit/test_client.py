e287f17659ae4130f19ddffe87e7277884c2add3
Alexander Schlarb <alexander@ninetailed.ninja>
2019-05-13 12:24:44 +0200

    Blacklist version 0.4.20 as it has several API issues (latest master of 0.4.21 has all of these fixed)

--- test/unit/test_client.py
+++ test/unit/test_client.py
@@ -5,12 +5,16 @@ import ipfshttpclient
 
 def test_assert_version():
 	# Minimum required version
-	ipfshttpclient.assert_version("0.1.0", "0.1.0", "0.2.0")
+	ipfshttpclient.assert_version("0.1.0", "0.1.0", "0.2.0", ["0.1.2"])
 	
 	# Too high version
 	with pytest.raises(ipfshttpclient.exceptions.VersionMismatch):
-		ipfshttpclient.assert_version("0.2.0", "0.1.0", "0.2.0")
+		ipfshttpclient.assert_version("0.2.0", "0.1.0", "0.2.0", ["0.1.2"])
 	
 	# Too low version
 	with pytest.raises(ipfshttpclient.exceptions.VersionMismatch):
-		ipfshttpclient.assert_version("0.0.5", "0.1.0", "0.2.0")
+		ipfshttpclient.assert_version("0.0.5", "0.1.0", "0.2.0", ["0.1.2"])
+	
+	# Blacklisted version
+	with pytest.raises(ipfshttpclient.exceptions.VersionMismatch):
+		ipfshttpclient.assert_version("0.1.2-1", "0.1.0", "0.2.0", ["0.1.2"])
