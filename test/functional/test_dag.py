2332800e0a4e00e81236dce9eab2d885ca171a7e
Joao Meyer <1994meyer@gmail.com>
2020-08-18 23:33:35 -0300

    Created base tests interface

---
+++ test/functional/test_dag.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+def test_export(client):
+    assert 0
+
+
+def test_get(client):
+    assert 0
+
+
+def test_import_(client):
+    assert 0
+
+
+def test_put(client):
+    assert 0
+
+
+def test_resolve(client):
+    assert 0
