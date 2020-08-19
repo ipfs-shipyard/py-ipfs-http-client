99f46f268d56c0b8937b141428db59c6cb4c9ea9
Joao Meyer <1994meyer@gmail.com>
2020-08-19 00:49:59 -0300

    Converted to tabs

--- ipfshttpclient/client/dag.py
+++ ipfshttpclient/client/dag.py
@@ -8,18 +8,18 @@ from .. import utils
 
 
 class Section(base.SectionBase):
-    @base.returns_single_item(base.ResponseBase)
-    def get(self, cid: str, **kwargs: base.CommonArgs):
-        args = (str(cid),)
-        return self._client.request('/dag/get', args, decoder='json', **kwargs)
+	@base.returns_single_item(base.ResponseBase)
+	def get(self, cid: str, **kwargs: base.CommonArgs):
+		args = (str(cid),)
+		return self._client.request('/dag/get', args, decoder='json', **kwargs)
 
-    @base.returns_single_item(base.ResponseBase)
-    def put(self, data: ty.IO, **kwargs: base.CommonArgs):
-        body, headers = multipart.stream_files(data, chunk_size=self.chunk_size)
-        return self._client.request('/dag/put', decoder='json', data=body,
-                                     headers=headers, **kwargs)
+	@base.returns_single_item(base.ResponseBase)
+	def put(self, data: ty.IO, **kwargs: base.CommonArgs):
+		body, headers = multipart.stream_files(data, chunk_size=self.chunk_size)
+		return self._client.request('/dag/put', decoder='json', data=body,
+									 headers=headers, **kwargs)
 
-    @base.returns_single_item(base.ResponseBase)
-    def resolve(self, cid: str, **kwargs: base.CommonArgs):
-        args = (str(cid),)
-        return self._client.request('/dag/resolve', args, decoder='json', **kwargs)
+	@base.returns_single_item(base.ResponseBase)
+	def resolve(self, cid: str, **kwargs: base.CommonArgs):
+		args = (str(cid),)
+		return self._client.request('/dag/resolve', args, decoder='json', **kwargs)
--- test/functional/test_dag.py
+++ test/functional/test_dag.py
@@ -4,20 +4,20 @@ import pytest
 import conftest
 
 def test_put_get_resolve(client):
-    data = io.BytesIO(br'{"links": []}')
-    response = client.dag.put(data)
+	data = io.BytesIO(br'{"links": []}')
+	response = client.dag.put(data)
 
-    assert 'Cid' in response
-    assert '/' in response['Cid']
-    assert response['Cid']['/'] == 'bafyreidepjmjhvhlvp5eyxqpmyyi7rxwvl7wsglwai3cnvq63komq4tdya'
+	assert 'Cid' in response
+	assert '/' in response['Cid']
+	assert response['Cid']['/'] == 'bafyreidepjmjhvhlvp5eyxqpmyyi7rxwvl7wsglwai3cnvq63komq4tdya'
 
-    response = client.dag.get('bafyreidepjmjhvhlvp5eyxqpmyyi7rxwvl7wsglwai3cnvq63komq4tdya')
+	response = client.dag.get('bafyreidepjmjhvhlvp5eyxqpmyyi7rxwvl7wsglwai3cnvq63komq4tdya')
 
-    assert 'links' in response
-    assert response['links'] == []
+	assert 'links' in response
+	assert response['links'] == []
 
-    response = client.dag.resolve('bafyreidepjmjhvhlvp5eyxqpmyyi7rxwvl7wsglwai3cnvq63komq4tdya')
+	response = client.dag.resolve('bafyreidepjmjhvhlvp5eyxqpmyyi7rxwvl7wsglwai3cnvq63komq4tdya')
 
-    assert 'Cid' in response
-    assert response['Cid']['/'] == 'bafyreidepjmjhvhlvp5eyxqpmyyi7rxwvl7wsglwai3cnvq63komq4tdya'
+	assert 'Cid' in response
+	assert response['Cid']['/'] == 'bafyreidepjmjhvhlvp5eyxqpmyyi7rxwvl7wsglwai3cnvq63komq4tdya'
 
