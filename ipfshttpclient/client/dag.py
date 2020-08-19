84c90c18fffc043e5ef626a0c531c8f7160fe2fc
Joao Meyer <1994meyer@gmail.com>
2020-08-19 00:29:24 -0300

    Implemented get

--- ipfshttpclient/client/dag.py
+++ ipfshttpclient/client/dag.py
@@ -8,20 +8,29 @@ from .. import utils
 
 
 class Section(base.SectionBase):
-    def export(self, cid: str):
-        pass
+    @base.returns_single_item(base.ResponseBase)
+    def export(self, cid: str, **kwargs: base.CommonArgs):
+        args = (str(cid),)
+        return self._client.request('/dag/export', args, decoder='json', **kwargs)
 
-    def get(self, ref: str):
-        pass
+    @base.returns_single_item(base.ResponseBase)
+    def get(self, cid: str, **kwargs: base.CommonArgs):
+        args = (str(cid),)
+        return self._client.request('/dag/get', args, decoder='json', **kwargs)
 
-    def import_(self, file: ty.IO, pin: bool = True):
-        pass
+    @base.returns_single_item(base.ResponseBase)
+    def import_(self, data: ty.IO, **kwargs: base.CommonArgs):
+        body, headers = multipart.stream_files(data, chunk_size=self.chunk_size)
+        return self._client.request('/dag/import', decoder='json', data=body,
+                                     headers=headers, **kwargs)
 
     @base.returns_single_item(base.ResponseBase)
     def put(self, data: ty.IO, **kwargs: base.CommonArgs):
-        body, headers = multipart .stream_files(data, chunk_size=self.chunk_size)
-        return self._client .request('/dag/put', decoder='json', data=body,
+        body, headers = multipart.stream_files(data, chunk_size=self.chunk_size)
+        return self._client.request('/dag/put', decoder='json', data=body,
                                      headers=headers, **kwargs)
 
-    def resolve(self, ref: str):
-        pass
+    @base.returns_single_item(base.ResponseBase)
+    def resolve(self, cid: str, **kwargs: base.CommonArgs):
+        args = (str(cid),)
+        return self._client.request('/dag/resolve', args, decoder='json', **kwargs)
--- test/functional/test_dag.py
+++ test/functional/test_dag.py
@@ -6,20 +6,22 @@ def test_export(client):
     assert 0
 
 
-def test_get(client):
-    assert 0
-
-
 def test_import_(client):
     assert 0
 
 
-def test_put(client):
+def test_put_get(client):
     data = io.BytesIO(br'{"links": []}')
     response = client.dag.put(data)
+
     assert 'Cid' in response
     assert '/' in response['Cid']
     assert response['Cid']['/'] == 'bafyreidepjmjhvhlvp5eyxqpmyyi7rxwvl7wsglwai3cnvq63komq4tdya'
+
+    response = client.dag.get('bafyreidepjmjhvhlvp5eyxqpmyyi7rxwvl7wsglwai3cnvq63komq4tdya')
+
+    assert 'links' in response
+    assert response['links'] == []
 
 
 def test_resolve(client):
