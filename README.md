adf308fdc2f29680bc14f0518ff87c83f0db49c1
Jan Rydzewski <flegmer@gmail.com>
2020-10-04 21:14:14 +0200

    Test with go-ipfs v0.7.0 and drop v0.4.22

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ unmaintained and does not work with any recent go-IPFS version.
 
 **Note**: This library occasionally has to change to stay compatible with the IPFS HTTP API.
 Currently, this library is tested against [go-ipfs v0.6.0](https://github.com/ipfs/go-ipfs/releases/tag/v0.6.0).
-We strive to support the last 5 releases of go-IPFS at any given time; go-IPFS v0.4.22 therefore
+We strive to support the last 5 releases of go-IPFS at any given time; go-IPFS v0.4.23 therefore
 being to oldest supported version at this time.
 
 ## Table of Contents
--- ipfshttpclient/client/__init__.py
+++ ipfshttpclient/client/__init__.py
@@ -14,9 +14,9 @@ import multiaddr
 DEFAULT_ADDR = multiaddr.Multiaddr(os.environ.get("PY_IPFS_HTTP_CLIENT_DEFAULT_ADDR", '/dns/localhost/tcp/5001/http'))
 DEFAULT_BASE = str(os.environ.get("PY_IPFS_HTTP_CLIENT_DEFAULT_BASE", 'api/v0'))
 
-VERSION_MINIMUM   = "0.4.22"
+VERSION_MINIMUM   = "0.4.23"
 VERSION_BLACKLIST = []
-VERSION_MAXIMUM   = "0.7.0"
+VERSION_MAXIMUM   = "0.8.0"
 
 from . import bitswap
 from . import block
@@ -67,7 +67,7 @@ def assert_version(version: str, minimum: str = VERSION_MINIMUM,
 
 	if minimum > version or version >= maximum:
 		raise exceptions.VersionMismatch(version, minimum, maximum)
-	
+
 	for blacklisted in blacklist:
 		blacklisted = list(map(int, blacklisted.split('-', 1)[0].split('.')))
 		if version == blacklisted:
@@ -77,16 +77,16 @@ def assert_version(version: str, minimum: str = VERSION_MINIMUM,
 def connect(
 		addr: http.addr_t = DEFAULT_ADDR,
 		base: str = DEFAULT_BASE, *,
-		
+
 		chunk_size: int = multipart.default_chunk_size,
 		offline: bool = False,
 		session: bool = False,
-		
+
 		auth: http.auth_t = None,
 		cookies: http.cookies_t = None,
 		headers: http.headers_t = {},
 		timeout: http.timeout_t = 120,
-		
+
 		# Backward-compat
 		username: ty.Optional[str] = None,
 		password: ty.Optional[str] = None
@@ -94,7 +94,7 @@ def connect(
 	"""Create a new :class:`~ipfshttpclient.Client` instance and connect to the
 	daemon to validate that its version is supported as well as applying any
 	known workarounds for the given daemon version
-	
+
 	Raises
 	------
 		~ipfshttpclient.exceptions.VersionMismatch
@@ -103,7 +103,7 @@ def connect(
 		~ipfshttpclient.exceptions.ProtocolError
 		~ipfshttpclient.exceptions.StatusError
 		~ipfshttpclient.exceptions.TimeoutError
-	
+
 	All parameters are identical to those passed to the constructor of the
 	:class:`~ipfshttpclient.Client` class.
 	"""
@@ -114,39 +114,39 @@ def connect(
 		auth=auth, cookies=cookies, headers=headers, timeout=timeout,
 		username=username, password=password,
 	)
-	
+
 	# Query version number from daemon and validate it
 	assert_version(client.apply_workarounds()["Version"])
-	
+
 	return client
 
 
 class Client(files.Base, miscellaneous.Base):
 	"""The main IPFS HTTP client class
-	
+
 	Allows access to an IPFS daemon instance using its HTTP API by exposing an
 	`IPFS Interface Core <https://github.com/ipfs/interface-ipfs-core/tree/master/SPEC>`__
 	compatible set of methods.
-	
+
 	It is possible to instantiate this class directly, using the same parameters
 	as :func:`connect`, to prevent the client from checking for an active and
 	compatible version of the daemon. In general however, calling
 	:func:`connect` should be preferred.
-	
+
 	In order to reduce latency between individual API calls, this class may keep
 	a pool of TCP connections between this client and the API daemon open
 	between requests. The only caveat of this is that the client object should
 	be closed when it is not used anymore to prevent resource leaks.
-	
+
 	The easiest way of using this “session management” facility is using a
 	context manager::
-	
+
 		with ipfshttpclient.connect() as client:
 			print(client.version())  # These calls…
 			print(client.version())  # …will reuse their TCP connection
-	
+
 	A client object may be re-opened several times::
-	
+
 		client = ipfshttpclient.connect()
 		print(client.version())  # Perform API call on separate TCP connection
 		with client:
@@ -155,22 +155,22 @@ class Client(files.Base, miscellaneous.Base):
 		with client:
 			print(client.version())  # These calls…
 			print(client.version())  # …will share a different TCP connection
-	
+
 	When storing a long-running :class:`Client` object use it like this::
-	
+
 		class Consumer:
 			def __init__(self):
 				self._client = ipfshttpclient.connect(session=True)
-			
+
 			# … other code …
-			
+
 			def close(self):  # Call this when you're done
 				self._client.close()
 	"""
-	
+
 	# Fix up docstring so that Sphinx doesn't ignore the constructors parameter list
 	__doc__ += "\n\n" + "\n".join(l[1:] for l in base.ClientBase.__init__.__doc__.split("\n"))
-	
+
 	bitswap   = base.SectionProperty(bitswap.Section)
 	block     = base.SectionProperty(block.Section)
 	bootstrap = base.SectionProperty(bootstrap.Section)
@@ -185,23 +185,23 @@ class Client(files.Base, miscellaneous.Base):
 	repo      = base.SectionProperty(repo.Section)
 	swarm     = base.SectionProperty(swarm.Section)
 	unstable  = base.SectionProperty(unstable.Section)
-	
-	
+
+
 	######################
 	# SESSION MANAGEMENT #
 	######################
-	
+
 	def __enter__(self):
 		self._client.open_session()
 		return self
-	
+
 	def __exit__(self, exc_type, exc_value, traceback):
 		self.close()
-	
+
 	def close(self):
 		"""Close any currently open client session and free any associated
 		resources.
-		
+
 		If there was no session currently open this method does nothing. An open
 		session is not a requirement for using a :class:`~ipfshttpclient.Client`
 		object and as such all method defined on it will continue to work, but
@@ -210,34 +210,34 @@ class Client(files.Base, miscellaneous.Base):
 		in the future. See the class's description for details.
 		"""
 		self._client.close_session()
-	
-	
+
+
 	###########
 	# HELPERS #
 	###########
-	
+
 	def apply_workarounds(self):
 		"""Query version information of the referenced daemon and enable any
 		   workarounds known for the corresponding version
-		
+
 		Returns
 		-------
 			dict
 				The version information returned by the daemon
 		"""
 		version_info = self.version()
-		
+
 		version = tuple(map(int, version_info["Version"].split('-', 1)[0].split('.')))
-		
+
 		self._workarounds.clear()
 		if version < (0, 5):  # pragma: no cover (workaround)
 			# Not really a workaround, but make use of HEAD requests on versions
 			# that support them to speed things up if we are not interested in the
 			# response anyways
 			self._workarounds.add("use_http_head_for_no_result")
-		
+
 		return version_info
-	
+
 	@utils.return_field('Hash')
 	@base.returns_single_item(dict)
 	def add_bytes(self, data: bytes, **kwargs):
@@ -309,8 +309,8 @@ class Client(files.Base, miscellaneous.Base):
 				Hash of the added IPFS object
 		"""
 		return self.add_bytes(encoding.Json().encode(json_obj), **kwargs)
-	
-	
+
+
 	@base.returns_single_item()
 	def get_json(self, cid, **kwargs):
 		"""Loads a json object from IPFS.
