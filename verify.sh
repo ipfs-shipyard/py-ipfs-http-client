e5121e5a71f0e206c92803e3ee512dbfcf6488c6
c0llab0rat0r <78339685+c0llab0rat0r@users.noreply.github.com>
2021-04-04 21:04:47 -0500

    Bump min Python version from 3.6.2 to 3.7.2

--- verify.sh
+++ verify.sh
@@ -10,7 +10,6 @@ if [ -z "$1" ]; then
     echo "Validating minimum point release of each supported minor version..."
 
     # Maintain this concurrently with [tool.flit.metadata].requires-python in pyproject.toml.
-    validate 3.6.2
     validate 3.7.2
     validate 3.8.0
     validate 3.9.0
