33107a6b0f1e8c2fd4f7f2efa29bd6f10cd22c3e
emendir <91378069+emendir@users.noreply.github.com>
2021-11-01 15:34:36 +0100

    Fixed docstrings.

--- ipfshttpclient/client/p2p.py
+++ ipfshttpclient/client/p2p.py
@@ -8,41 +8,44 @@ class Section(base.SectionBase):
     def forward(self, protocol: str, peer_id: str, port: str, **kwargs: base.CommonArgs):
         """Forward connections to libp2p service
 
-        Forward connections made to <listen-address> to <target-address>.
+        Forward connections made to the specified port to another IPFS node.
 
         .. code-block:: python
 
-                # forwards connections made to 'port' to 'QmHash'
-                >>> client.p2p.forward('protocol', 'port', 'QmHash')
+                # forwards connections made to port 8888 to 'QmHash' as protocol '/x/testproto'
+                >>> client.p2p.forward('/x/testproto', 'QmHash', 8888)
                 []
 
         Parameters
         ----------
         protocol
                 specifies the libp2p protocol name to use for libp2p connections and/or handlers. It must be prefixed with '/x/'.
+        PeerID
+                Target endpoint
         port
                 Listening endpoint
-PeerID
-                Target endpoint
 
         Returns
         -------
-                list
-                        An empty list
+        list
+                An empty list
         """
         args = (protocol, peer_id, port)
         return self._client.request('/p2p/forward', args, decoder='json', **kwargs)
 
     @base.returns_no_item
     def listen(self, protocol: str, port: str, **kwargs: base.CommonArgs):
-        """Create libp2p service
+        """Create libp2p service to forward IPFS connections to port
+
+        Creates a libp2p service that forwards IPFS connections to it
+        to the specified port on the local computer.
 
 
         .. code-block:: python
 
-                Create libp2p service and forward IPFS connections to 'port'
-                >>> client.p2p.listen('protocol', 'port')
-                []
+            # listens for connections of protocol '/x/testproto' and forwards them to port 8888
+            >>> client.p2p.listen('/x/testproto', 8888)
+            []
 
         Parameters
         ----------
@@ -54,22 +57,23 @@ PeerID
 
         Returns
         -------
-                list
-                        An empty list
+        list
+                An empty list
         """
         args = (protocol, port)
         return self._client.request('/p2p/listen', args, decoder='json', **kwargs)
 
     # @base.returns_single_item(base.ResponseBase)
     def close(self, all: bool = False, protocol: str = None, listenaddress: str = None, targetaddress: str = None, **kwargs: base.CommonArgs):
-        """Create libp2p service
+        """Stop listening for new connections to forward.
 
+        Stops all forwarding and listening libp2p services that match the input arguments.
 
         .. code-block:: python
 
-                Create libp2p service and forward IPFS connections to 'port'
-                >>> client.p2p.listen('protocol', 'port')
-                []
+            # Close listening and forwarding connections of protocol '/x/testproto' and port 8888.
+            >>> client.p2p.close(protocol='/x/testproto', port='8888')
+            []
 
         Parameters
         ----------
@@ -98,3 +102,4 @@ PeerID
         kwargs.setdefault("opts", {}).update(opts)
         args = (all,)  # if all is not None else ()
         return self._client.request('/p2p/close', decoder='json', **kwargs)
+
